0639227de154126c86cc1ca4f8286492b75bad56
Shrinjay <shrinjaymukherjee@gmail.com>
2020-11-17 18:31:21 -0700

    Added base class for decklinksrc

---
+++ modules/decklinksrc/decklinksrc.py
@@ -0,0 +1,17 @@
+import cv2 as cv2
+import numpy as np
+
+class DeckLinkSRC:
+
+    def __init__(self):
+        self.currentFrame
+        self.capture = cv2.VideoCapture('decklinkvideosrc mode=7 connection=0 ! videoconvert ! appsink') #Starts capture on initialization of object
+        #Because of this, we no longer need start stream code from Aryan
+
+    def stop(self): #Logic for stopping video feed by releasing capture and destroying any windows open.
+
+    
+    def grab(self): #Logic for grabbing frame from deckLink
+
+
+    
