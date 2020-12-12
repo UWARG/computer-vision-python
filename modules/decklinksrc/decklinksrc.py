7889b51245e23d233d9fc42013e4e6d423372c4e
Aryan Kalia <kalia.aryan2013@gmail.com>
2020-12-12 15:36:25 -0500

    gitignore edit pycache

--- modules/decklinksrc/decklinksrc.py
+++ modules/decklinksrc/decklinksrc.py
@@ -4,7 +4,7 @@ import numpy as np
 class DeckLinkSRC:
 
     def __init__(self):
-        self.currentFrame = None
+        self.__currentFrame = None
         self.capture = cv2.VideoCapture('decklinkvideosrc mode=7 connection=0 ! videoconvert ! appsink') #Starts capture on initialization of object
         #Because of this, we no longer need start stream code from Aryan
 
@@ -12,11 +12,11 @@ class DeckLinkSRC:
         self.capture.release()
         cv2.destroyAllWindows()
     
-    def grab(self): #Logic for grabbing frame from deckLink
-        cv2.imshow('Grabbedframe', self.currentFrame)
+    def grab(self): #Logic for displaying single frame from deckLink
+        cv2.imshow('Grabbedframe', self.__currentFrame)
     
     def get_frame(self): #Logic for returning the current frame as a numpy array
-        return self.currentFrame
+        return self.__currentFrame
     
     def quitProgram(self):
         self.capture.release()
@@ -25,7 +25,7 @@ class DeckLinkSRC:
     def display(self):
         while(True):
             ret, frame = self.capture.read()
-            self.currentFrame = frame
+            self.__currentFrame = frame
             cv2.imshow('VideoStream', frame)
 
             #OpenCV doesn't allow you to access a camera without a camera release, So feel free to replace this bottom with however the video stream will quit (right now it quits on spacebar)
