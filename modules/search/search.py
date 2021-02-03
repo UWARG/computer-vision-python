c89e1df6128a108da9da896619c77d91cea1a153
Andy <andy999coc@gmail.com>
2021-02-02 21:03:14 -0500

    search program

--- modules/search/search.py
+++ modules/search/search.py
@@ -1,51 +1,38 @@
-import geopy
-from numpy import arctan2,random,sin,cos,degrees
-
-def searchProgram(tent_GPS, plane_GPS, heading):
-  '''
-  tent_GPS and plane_GPS are dictionaries with lat and lon values
-  '''
-  
-  '''
-  Calculating Bearing between Tent and Plane
-  '''
-  # STEP 1: Calculating Difference in Longitude
-  dL = tent_GPS.lon - plane_GPS.lon
-
-  # STEP 2: Converting Longitude and Latitude to corresponding XY coordinates
-  X = cos(tent_GPS.lat)* sin(dL)
-  Y = cos(plane_GPS.lat)*sin(tent_GPS.lat) - sin(plane_GPS.lat)*cos(tent_GPS.lat)* cos(dL)
-
-  # Step 3: Calculate Bearings in Radians and then converting to degrees
-  bearing = (degrees(arctan2(X,Y))+360) % 360
-
-  
-  '''
-  Calculating Angle of Rotation
-  '''
-  cc = False
-  rotate = 0.0
-  
-  # Step 1: Calculate the difference in Bearings:
-  dB = bearing - heading
-
-  # Step 2: Check for smallest rotation
-  if dB > 0:
-    if dB > 180:
-      dB = 360-dB
-      cc = True
-    rotate = dB
-  
-  elif dB < 0:
-    if dB < -180:
-      dB = 360+dB
-    else:
-      dB *= -1
-      cc = True
-    rotate = dB
+import math
+import numpy as np
+from numpy import sin, cos
 
 
-''' REACHING HERE:
-        rotate (float): stores angle of rotation
-        cc (bool): stores if rotation should be counterclockwise (cc == True) or not
-'''
+def altSearch(tent_GPS, plane_GPS, angle):
+    """Search program to determine the minimum rotation angle between heading and tent
+
+    Parameters
+    ----------
+    tent_GPS : obj
+      dictionary containing lat and long coordinates of the tent
+    plane_GPS : obj
+      dictionary containing lat and long coordinate of the plane
+    angle : in
+      bearings notation in radians
+
+    Returns
+    -------
+    int
+      returns the minimum rotation angle
+
+    """
+    angle = math.radians(angle)
+    # vector of the plane's current heading
+    v_heading = np.array([cos(angle), sin(angle)])
+    # vector from plane to tent
+    v_plane_tent = np.array(
+        [tent_GPS["lat"] - plane_GPS["lat"], tent_GPS["lon"] - plane_GPS["lon"]])
+
+    # dot product of vectors
+    dot = np.dot(v_heading, v_plane_tent)
+
+    # find the angle between the two vectors
+    rotation = math.acos(dot/(np.linalg.norm(v_heading)
+                              * np.linalg.norm(v_plane_tent)))
+    print(math.degrees(rotation))
+    return rotation
