e2e4ee027c4fd6b08fb675537b15271b58a1938b
AddeshB <71418846+AddeshB@users.noreply.github.com>
2021-02-04 08:45:02 -0500

    Update videoMediator.py

--- modules/videoMediator/videoMediator.py
+++ modules/videoMediator/videoMediator.py
@@ -1,1 +1,20 @@
+import TargetAcquisition
+import DecklinkSRC
 
+class VideoMediator:
+    tent_coordinates = None
+    decklinksrc=DecLinkSRC()
+    target_acquisition=TargetAcquisition()
+
+    def __init__(self):
+        self.__current_frame=None
+
+        """
+        initialize thread
+
+        """
+
+
+    def getTargets(self):
+        current_frame=decklinksrc.grab()
+        tent_coordinates=target_acquisition.get_coordinates(current_frame)
