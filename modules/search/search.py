d5fb3b4e7a06900f87baa2cb90d1630ff23a78af
Andy <andy999coc@gmail.com>
2021-02-09 19:50:28 -0500

    search edits

--- modules/search/search.py
+++ modules/search/search.py
@@ -3,36 +3,43 @@ import numpy as np
 from numpy import sin, cos
 
 
-def altSearch(tent_GPS, plane_GPS, angle):
-    """Search program to determine the minimum rotation angle between heading and tent
+class Search:
+    """Performs search algorithm"""
 
-    Parameters
-    ----------
-    tent_GPS : obj
-      dictionary containing lat and long coordinates of the tent
-    plane_GPS : obj
-      dictionary containing lat and long coordinate of the plane
-    angle : in
-      bearings notation in radians
+    def __init__(self):
+        pass
 
-    Returns
-    -------
-    int
-      returns the minimum rotation angle
+    def perform_search(self, tentGPS, planeGPS, angle):
+        """Search program to determine the minimum rotation angle between heading and tent
 
-    """
-    angle = math.radians(angle)
-    # vector of the plane's current heading
-    v_heading = np.array([cos(angle), sin(angle)])
-    # vector from plane to tent
-    v_plane_tent = np.array(
-        [tent_GPS["lat"] - plane_GPS["lat"], tent_GPS["lon"] - plane_GPS["lon"]])
+        Parameters
+        ----------
+        tentGPS : obj
+          dictionary containing lat and long coordinates of the tent
+        planeGPS : obj
+          dictionary containing lat and long coordinate of the plane
+        angle : float
+          bearings notation in degrees
 
-    # dot product of vectors
-    dot = np.dot(v_heading, v_plane_tent)
+        Returns
+        -------
+        int
+          returns the minimum rotation angle
 
-    # find the angle between the two vectors
-    rotation = math.acos(dot/(np.linalg.norm(v_heading)
-                              * np.linalg.norm(v_plane_tent)))
-    print(math.degrees(rotation))
-    return rotation
+        """
+        # converts heading degrees into radians
+        heading = math.radians(angle)
+        # vector of the plane's current heading
+        vHeading = np.array([cos(heading), sin(heading)])
+        # vector from plane to tent
+        vPlaneTent = np.array(
+            [tentGPS["lat"] - planeGPS["lat"], tentGPS["lon"] - planeGPS["lon"]])
+
+        # dot product of vectors
+        dot = np.dot(vHeading, vPlaneTent)
+
+        # find the angle between the two vectors
+        rotation = math.acos(dot/(np.linalg.norm(vHeading)
+                                  * np.linalg.norm(vPlaneTent)))
+        print(math.degrees(rotation))
+        return rotation
