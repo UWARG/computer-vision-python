2b4d0e4d89d722a1aea85e746d651f571c8ab9f9
AddeshB <71418846+AddeshB@users.noreply.github.com>
2021-02-11 20:55:06 -0500

    Update call train.py

--- call train.py
+++ call train.py
@@ -1,6 +1,11 @@
 import os
 
 def _main_():
+    main_directory=os.getcwd()
+"""
+stores current working directory prior to change
+
+"""
     if os.path.exists("targetAcquisition/yolov2_assets"):
         os.chdir("targetAcquisition/yolov2_assets")
 """
@@ -9,7 +14,7 @@ Changing directory to yolov2_assets to get config.json
 """
         from yolov2_assets import train
         train(config.json)
-        os.chdir("main.py")
+        os.chdir(main_directory)
     else :
         print ("YOLOV2_ASSETS Directory not found. Specify path")
 if _name_ == 'main':
