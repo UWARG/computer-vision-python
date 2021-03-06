034de01b1fcfbdae4de55d268adb9eb514260a73
rayjinghaolei <rayjinghaolei@gmail.com>
2021-03-06 17:35:56 -0500

    5 box detection update
    1. removed all parameters needed when calling and hard coded in it
    2. records all the box coordinates in the frame and return the function when the box number hits 5

--- modules/targetAcquisition/taxi/boxDetection/detect.py
+++ modules/targetAcquisition/taxi/boxDetection/detect.py
@@ -17,12 +17,32 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 def detect(save_img=False):
-    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://'))
-
+    
+    #important ones
+    conf_thres=0.4
+    img_size=416
+    iou_thres=0.45
+    source='0'
+    weights=['./weights/best.pb']
+    #less important ones
+    agnostic_nms=False
+    augment=False
+    classes=[0]
+    device=''
+    exist_ok=False
+    name='exp'
+    project='runs/detect'
+    save_conf=False
+    save_txt=False
+    tfl_int8=False
+    update=False
+    view_img=False
+    #other variables
+    webcam = True
+    imgsz = img_size
+    
     # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    save_dir = Path(increment_path(Path(project) / name, exist_ok=exist_ok))  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Initialize
@@ -95,6 +115,9 @@ def detect(save_img=False):
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if (device.type != 'cpu' and backend == 'pytorch') else None  # run once
     for path, img, im0s, vid_cap in dataset:
+        #variable for return bounding box coordinates
+        bbox_cord = []
+        
         img = torch.from_numpy(img).to(device)
         img = img.half() if half and backend == 'pytorch' else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -104,7 +127,7 @@ def detect(save_img=False):
         # Inference
         t1 = time_synchronized()
         if backend == 'pytorch':
-            pred = model(img, augment=opt.augment)[0]
+            pred = model(img, augment=augment)[0]
         else:
             if backend == 'saved_model':
                 pred = model(img.permute(0, 2, 3, 1).cpu().numpy(), training=False).numpy()
@@ -112,11 +135,12 @@ def detect(save_img=False):
                 pred = frozen_func(x=tf.constant(img.permute(0, 2, 3, 1).cpu().numpy())).numpy()
             
             # Denormalize xywh
-            pred[..., :4] *= opt.img_size
+            pred[..., :4] *= img_size
             pred = torch.tensor(pred)
-
+            
+        
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
         t2 = time_synchronized()
 
         # Apply Classifier
@@ -148,21 +172,26 @@ def detect(save_img=False):
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                        bbox_cord.append(plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3))
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
-
+            print(bbox_cord)
+            if len(bbox_cord) == 5:
+                return bbox_cord
+            
             # Stream results
             if view_img:
                 cv2.imshow(str(p), im0)
-
+                
+            
+        
             # Save results (image with detections)
             if save_img:
                 if dataset.mode == 'image':
@@ -185,35 +214,8 @@ def detect(save_img=False):
         print(f"Results saved to {save_dir}{s}")
 
     print(f'Done. ({time.time() - t0:.3f}s)')
+    return bbox_cord
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--tfl-int8', action='store_true', help='use int8 quantized TFLite model')
-    opt = parser.parse_args()
-    print(opt)
-    check_requirements()
-
-    with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
+    print(detect())
--- modules/targetAcquisition/taxi/boxDetection/utils/plots.py
+++ modules/targetAcquisition/taxi/boxDetection/utils/plots.py
@@ -60,16 +60,17 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    print("top left x: " + str(int(x[0])))
+    ''' print("top left x: " + str(int(x[0])))
     print("top left y: " + str(int(x[1])))
     print("bot right x: " + str(int(x[2])))
-    print("bot right y: " + str(int(x[3])))
+    print("bot right y: " + str(int(x[3]))) '''
     if label:
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    return ((int(x[0]),int(x[1])),(int(x[2]),int(x[3])))
 
 
 def plot_wh_methods():  # from utils.plots import *; plot_wh_methods()
