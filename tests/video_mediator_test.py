97fcb25d7ed325fadacda935e99af544fbf0c9a1
shrinjay <shrinjaymukherjee@gmail.com>
2021-03-24 23:26:42 -0600

    Updated comment

--- tests/video_mediator_test.py
+++ tests/video_mediator_test.py
@@ -1,8 +1,9 @@
 import pytest
 from modules.videoMediator.videoMediator import VideoMediator
-from time import sleep
 
 
+# Integration test to check mediator behaviour
 def testVideoMediator():
     videoMediator = VideoMediator(testMode=True)
+    # By default if an empty frame is passed into the targetAcquisition class, "False" will be put into the pipeline
     assert videoMediator.get_coordinates() == [False]
