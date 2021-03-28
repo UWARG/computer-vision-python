1cdd58a46c6777930afec6e9f27f0055fb1e1d17
Aryan Kalia <kalia.aryan2013@gmail.com>
2021-03-28 14:31:04 -0400

    more readable implementation

--- modules/geolocation/geolocation.py
+++ modules/geolocation/geolocation.py
@@ -96,6 +96,14 @@ class Geolocation:
 
         return pixelGeoPairs
     
+    def __are_three_points_collinear(self, p1, p2, p3):
+        x = 0
+        y = 1
+        return 0.5*(p1[x]*(p2[y]-p3[y]) +
+                    p2[x]*(p3[y]-p1[y]) +
+                    p3[x]*(p1[y]-p2[y])) == 0
+
+
     def get_non_collinear_points(self, coordinatesArray):
         """
         Returns a list of four coordinates from an input array that are not collinear to one another.
@@ -116,18 +124,20 @@ class Geolocation:
         if len(coordinatesArray) < 4:
             return collinearPoints
 
-        for i in range(0, len(coordinatesArray)-3):
-            x1, y1 = coordinatesArray[i]
-            x2, y2 = coordinatesArray[i+1]
-            x3, y3 = coordinatesArray[i+2]
-            x4, y4 = coordinatesArray[i+3]
+        for i in range(0, len(coordinatesArray)):
+            points = np.zeros(shape=(4, 2))
 
-            if (0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)) != 0 and
-                0.5*(x2*(y3-y4)+x3*(y4-y2)+x4*(y2-y3)) != 0 and
-                0.5*(x3*(y4-y1)+x4*(y1-y3)+x1*(y3-y4)) != 0 and
-                0.5*(x4*(y1-y2)+x1*(y2-y4)+x2*(y4-y1)) != 0):
-                
-                collinearPoints = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
+            for j in range(0, 4):
+                points[j] = coordinatesArray[(i+j) % len(coordinatesArray)]
+
+            areFourCollinear = True
+            for i in range(0, 4):
+                areFourCollinear &= not self.__are_three_points_collinear(points[i], 
+                                                                          points[(i+1) % 4],
+                                                                          points[(i+2) % 4])
+            
+            if areFourCollinear:
+                collinearPoints = points
                 break
 
         return collinearPoints
