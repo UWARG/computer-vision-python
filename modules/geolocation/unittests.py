f4cae7c0bd6e21c6dcccdaae2bfa661b12d5e0a2
Bassel Al Omari <b2alomar@uwaterloo.ca>
2021-03-28 20:00:39 +0300

    Added Two More Unit Tests
    - Second Set of Random Transformation Matrix and Pixel Coordinates
    - Tested scenario when the homogenized z coordinate equals 0

--- modules/geolocation/unittests.py
+++ modules/geolocation/unittests.py
@@ -219,17 +219,35 @@ class TestMapLocationFromPixel(unittest.TestCase):
         # Test
         np.testing.assert_almost_equal(actual, expected)
     
-    def test_transformation_matrix_1(self):
+    def test_set_1_int(self):
 
         # Setup
         transformationMatrix = np.array([[4,6,152],[120,5,99],[3,5,2]])
         pixelCoordinates = np.array([[2,3],[12,99],[623,126],[1604,12],[0,4]])
 
-        expected = np.array([[7.739130435,15.39130435], 
-                                [1.489681051,3.816135084], 
-                                [1.359456218,30.18352659],
-                                [1.362330735,39.52379975],
-                                [8,5.409090909]])
+        expected = np.array([[7.739130435, 15.39130435], 
+                             [1.489681051, 3.816135084], 
+                             [1.359456218, 30.18352659],
+                             [1.362330735, 39.52379975],
+                             [8,5.409090909]])
+
+        # Run
+        actual = self.locator.map_location_from_pixel(transformationMatrix, pixelCoordinates)
+
+        # Test
+        np.testing.assert_almost_equal(actual, expected)
+    
+    def test_set_2_float(self):
+
+        # Setup
+        transformationMatrix = np.array([[66.3413,23.4231,12.8855],[95.9351,13.1522,9.5166],[3.9963,5.1629,2.6792]])
+        pixelCoordinates = np.array([[61,3],[33,99],[46,26],[72,66],[94,77]])
+
+        expected = np.array([[15.76673823, 22.52792524], 
+                             [7.00192958,  6.93441577], 
+                             [11.45331267, 14.85447104],
+                             [10.03761573, 12.3341739],
+                             [10.37866862, 12.94040829]])
 
         # Run
         actual = self.locator.map_location_from_pixel(transformationMatrix, pixelCoordinates)
@@ -243,11 +261,25 @@ class TestMapLocationFromPixel(unittest.TestCase):
         transformationMatrix = np.array([[0.2231,0.1222,0.0345],[0.0512,0.0041,0.0062],[0.3315,0.8720,0.1261]])
         pixelCoordinates = np.array([[2,3],[12,99],[623,126],[1604,12],[0,4]])
 
-        expected = np.array([[0.24883274,0.03550557], 
-                             [0.16376375,0.01135106], 
-                             [0.48787354,0.10242681],
-                             [0.66262702,0.15153561],
-                             [0.14479400,0.00625329]])
+        expected = np.array([[0.24883274, 0.03550557], 
+                             [0.16376375, 0.01135106], 
+                             [0.48787354, 0.10242681],
+                             [0.66262702, 0.15153561],
+                             [0.14479400, 0.00625329]])
+
+        # Run
+        actual = self.locator.map_location_from_pixel(transformationMatrix, pixelCoordinates)
+
+        # Test
+        np.testing.assert_almost_equal(actual, expected)
+
+    def test_homogenized_z_equals_0_case(self):
+
+        # Setup
+        transformationMatrix = np.array([[5,1,5],[9,1,2],[0,0,0]])
+        pixelCoordinates = np.array([[5,9],[2,5],[3,3],[11,3],[7,1]])
+
+        expected = np.zeros(shape = (5,2))
 
         # Run
         actual = self.locator.map_location_from_pixel(transformationMatrix, pixelCoordinates)
