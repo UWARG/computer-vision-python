451897695948d33b6c561556a7ef40538a1f3c81
Xierumeng <xierumeng@hotmail.com>
2021-04-09 20:03:57 -0400

    counter comments

--- modules/multiprocessExample/counter.py
+++ modules/multiprocessExample/counter.py
@@ -8,18 +8,16 @@ A producer-consumer will have both an input and output, shouldn't be too hard
 
 class Counter:
     """
-    Counts from 0 to a provided maximum and uses a thousands digit
-    Producer: Places current count in the provided queue
+    Increments its internal counter
+    Producer: Outputs current count
     """
 
-    # These are all static (global) variables
     # They're all public in this example, but should probably be private
-    # The manager class would be a "friend", in C++ parlance
-
-    # Globals, if not initialized directly here, must be initialized before the process is started
+    # Globals must be initialized here ("compile"-time)
+    # They could be initialized at runtime before starting the process but this is HIGHLY DISCOURAGED
     maxCount = 999
 
-    # Class must be constructed before the process is started
+
     def __init__(self, thousands):
 
         self.numberToAdd = thousands * 1000
