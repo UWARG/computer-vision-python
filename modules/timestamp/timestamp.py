f39be9901ac0ef350ab056b0546de73fb4876582
Wenfei134 <60166421+Wenfei134@users.noreply.github.com>
2021-05-11 15:52:08 -0700

    removed getter methods, changed name of attribute to data

--- modules/timestamp/timestamp.py
+++ modules/timestamp/timestamp.py
@@ -7,26 +7,16 @@ class Timestamp:
     ...
     Attributes
     ----------
-    frame : 
+    data : 
     timestamp : datetime.datetime
 
     Methods
     -------
-    __init__(frame, timestamp (optional, defaults to time of initialization)) 
+    __init__(data, timestamp (optional, defaults to time of initialization)) 
         sets the frame and timestamp
-    get_frame  
-        returns the frame
-    get_timestamp 
-        returns the timestamp
 
     """
-    def __init__(self, frame, timestamp = datetime.datetime.now()):
-        self.frame  = frame
+    def __init__(self, data, timestamp = datetime.datetime.now()):
+        self.data = data
         self.timestamp = timestamp
-
-    def get_frame(self):
-        return self.frame
-
-    def get_timestamp(self):
-        return self.timestamp
 
