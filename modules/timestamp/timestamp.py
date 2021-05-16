6aa8bf8d3ec9f1dcd0751d8cc2d5067a49ac0362
Wenfei134 <60166421+Wenfei134@users.noreply.github.com>
2021-05-16 13:03:40 -0700

    Added a merge and get_average to help later on in the pipeline

--- modules/timestamp/timestamp.py
+++ modules/timestamp/timestamp.py
@@ -1,3 +1,5 @@
+from __future__ import annotations 
+
 import datetime as datetime
 import numpy.typing as npt
 import typing
@@ -10,13 +12,19 @@ class Timestamp:
     ...
     Attributes
     ----------
-    data : 
+    data : Union[npt.ArrayLike, dict]
     timestamp : datetime.datetime
 
     Methods
     -------
-    __init__(data, timestamp (optional, defaults to time of initialization)) 
-        sets the frame and timestamp
+    __init__(data : np.ArrayLike, timestamp : datetime.datetime, optional) 
+        sets the data and timestamp, timestamp defaults to time of initialization
+    __get_average_time(self, otherTime : datetime.datetime)
+        returns the average of the self.timestamp and the input datetime
+    merge(self, other : Timestamp)
+        merges two Timestamp objects in the following way:
+            data is a tuple of both datas (self.data, other.data)
+            timestamp is the average of the two times 
 
     """
     def __init__(self, data: typing.Union[npt.ArrayLike, dict], timestamp: datetime.datetime = None):
@@ -25,3 +33,14 @@ class Timestamp:
         if timestamp is None: 
             self.timestamp = datetime.datetime.now()
 
+    def __get_average_time(self, otherTime: datetime.datetime) -> datetime.datetime:
+        deltaOne = self.timestamp - datetime.datetime.fromtimestamp(0)
+        deltaTwo = otherTime - datetime.datetime.fromtimestamp(0)
+
+        averageDelta = (deltaOne + deltaTwo) / 2 
+        averageTime = datetime.datetime.fromtimestamp(averageDelta / datetime.timedelta(seconds=1))
+        return averageTime
+
+    def merge(self, other: typing.type[Timestamp]):
+        self.data = (self.data, other.data)
+        self.timestamp = self.__get_average_time(other.timestamp)
