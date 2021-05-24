4f8a61d821dab6ac9cebf31e20f6792f1bfe0b34
Aryan K <37564506+manumanuk@users.noreply.github.com>
2021-05-24 13:45:25 -0400

    implementation fix

--- modules/QRScanner/QR.py
+++ modules/QRScanner/QR.py
@@ -1,4 +1,4 @@
-import pyzbar from pyzbar
+from pyzbar import pyzbar
 import cv2
 
 class QRScanner:
@@ -6,23 +6,24 @@ class QRScanner:
         self.codes = []
 
     def main(self, frame):
-        self.codes = self.get_qr_codes(frame)
+        self.codes = []
+        self.get_qr_codes(frame)
         return self.draw_qr_codes(frame)
 
     def get_qr_codes(self, frame):
         qrCodes = pyzbar.decode(frame)
         
         for qr in qrCodes:
+            (x, y, w, h) = qr.rect
+            
             text = '{}: {}'.format(qr.type, qr.data.decode("utf-8"))
-            self.codes.append({"text": text, "rect": qr.rect})
-        
+            self.codes.append({"text": text, "rect": (x, y, w, h)})
         return frame
 
     def draw_qr_codes(self, frame):
-        
         for qr in self.codes:
-            (x, y, w, h) = qr.rect
+            (x, y, w, h) = qr["rect"]
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            cv2.putText(frame, qr.text, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            cv2.putText(frame, qr["text"], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
         return frame
