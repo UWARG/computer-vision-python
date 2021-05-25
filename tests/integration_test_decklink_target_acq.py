0a535b97dc860e62ae0a4e4908441cef67e47085
rayjinghaolei <rayjinghaolei@gmail.com>
2021-05-25 17:14:22 -0400

    Create integration_test_decklink_target_acq.py

---
+++ tests/integration_test_decklink_target_acq.py
@@ -0,0 +1,16 @@
+import pytest
+import cv2
+from modules.targetAcquisition.targetAcquisition import TargetAcquisition
+from modules.decklinksrc.decklinksrc import DeckLinkSRC
+
+def testTargetAcquisition():
+    decklinkSrc = DeckLinkSRC()
+    tracker = TargetAcquisition()
+    while True:
+        
+        curr_frame = decklinkSrc.grab()
+        assert curr_frame != None
+        decklinkSrc.display()
+        tracker.set_curr_frame(curr_frame)
+        assert tracker.get_coordinates() != None
+    
