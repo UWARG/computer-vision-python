fc6f64fbb3dcf18448056f6af8f2cf84da84040c
ksisjaya <ksisjaya@uwaterloo.ca>
2021-05-27 14:12:47 -0400

    Add worker function for POGI data

--- modules/commandModule/commandFns.py
+++ modules/commandModule/commandFns.py
@@ -1,6 +1,7 @@
 from modules.commandModule.commandModule import CommandModule
 
 PIGO_DIR = ""
+POGI_DIR = ""
 
 def write_pigo(newPigo):
 #Boolean to check if any changes were made
@@ -32,3 +33,29 @@ def write_pigo(newPigo):
 #if nothing has changed, return the dictionary that was passed in
     else:
         return
+
+def read_pogi() -> dict:
+
+    """
+    Reads all data currently in the POGI file and returns as a dict
+
+    Returns
+    -------
+    dict:
+        contains pogiData as specified in commandModule
+    """
+
+    command = CommandModule(pigoFileDirectory=PIGO_DIR, pogiFileDirectory=POGI_DIR)
+
+    # store all current POGI data into a dict
+    # note: the keys included in the dict are those which are currently valid and working in commandModule
+    #       (some fields are defined in the wiki but are currently commented out)
+    pogiData =  { 'currentAltitude' : command.get_current_altitude(),
+                  'currentAirspeed' : command.get_current_airspeed(),
+                  'isLanded' : command.get_is_landed(),
+                  'eulerAnglesOfCamera' : command.get_euler_angles_of_camera(),
+                  'eulerAnglesOfPlane' : command.get_euler_angles_of_plane(),
+                  'gpsCoordinates' : command.get_gps_coordinates(),
+                }
+    
+    return pogiData
--- modules/commandModule/commandWorker_flight.py
+++ modules/commandModule/commandWorker_flight.py
@@ -1,4 +1,8 @@
-from modules.commandModule.commandFns import write_pigo
+from modules.commandModule.commandModule import CommandModule
+from modules.commandModule.commandFns import write_pigo, read_pogi
+
+POGI_DIR = ""
+PIGO_DIR = ""
 
 def flight_command_worker(pipelineIn, pipelineOut):
 	# pipelineIn gives [[x,y], [range]]
@@ -15,4 +19,34 @@ def flight_command_worker(pipelineIn, pipelineOut):
 
 	#TODO: Write logic to get POGI
 
-	
+
+def pogi_command_worker(pause, exitRequest, pipelineOut):
+    """
+    Producer worker function to return data currently in the POGI file
+
+    Parameters
+    ----------
+    pause : multiprocessing.Lock
+        a lock shared between worker processes
+    exitRequest : multiprocessing.Queue
+        a queue that determines when the worker process stops
+    pipelineOut : multiprocessing.Queue
+        output pipeline for pogiData
+    """
+
+    print("Reading POGI data")
+
+    while True:
+        pause.acquire()
+        pause.release()
+
+        # get the pogi data
+        pogiData = read_pogi()
+
+        # output to pipeline
+        # note: no null checking required since CommandModule returns None for fields that are unreadable/inaccesible
+        #       if all fields are None, then pogiData will be a dict with all elements as {'example_key' : None}
+        pipelineOut.put(pogiData)
+
+        if not exitRequest.empty():
+            return
