aefb4eeb2329bdf1da326dd0b5f024b80b02e423
Shrinjay Mukherjee <62576642+Shrinjay@users.noreply.github.com>
2021-06-10 18:15:30 -0600

    Update commandWorker_flight.py

--- modules/commandModule/commandWorker_flight.py
+++ modules/commandModule/commandWorker_flight.py
@@ -1,17 +1,20 @@
-from modules.commandModule.commandFns import write_pigo
+from modules.commandModule.commandModule import CommandModule
+
+PIGO_DIR = ""
 
 def flight_command_worker(pipelineIn, pipelineOut):
+	command = CommandModule(pigoFileDirectory=PIGO_DIR)
 	# pipelineIn gives [[x,y], [range]]
 	if pipelineIn.empty:
 	   continue
 	
 	data = pipelineIn.get()
 	# Cache data here
-	newPigo = {
-		'gpsCoordinates': [x, y]
+	ground_command = {
+		'gpsCoordinates': [data[0], data[1]]
 	}
 
-	write_pigo(newPigo)
+	command.set_gps_coordinates(ground_command)
 
 	#TODO: Write logic to get POGI
 
