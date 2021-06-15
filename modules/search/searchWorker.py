77438adc5be2683f25eb77cc0f84a8e1bc3193ea
FizzesShark <xbug2001@gmail.com>
2021-06-14 22:50:45 -0700

    Fixed indentation issues

--- modules/search/searchWorker.py
+++ modules/search/searchWorker.py
@@ -2,10 +2,10 @@ from modules.search.Search import Search
 
 
 def searchWorker(pause, exitRequest, pipelineIn, pipelineOut):
-     """
+    """
         Search worker process to perform search with multiprocessing
         Gets data from the input pipeline, performs search, and puts it into the output pipeline
-        
+
         Parameters
         ----------
         pause : multiprocessing.Lock
@@ -16,27 +16,26 @@ def searchWorker(pause, exitRequest, pipelineIn, pipelineOut):
           input pipeline
         pipelineOut : multiprocessing.Queue
           output pipeline
-        
+
         Returns
         -------
         None
-        """
-  
+    """
     print("Start Search")
-    
+
     search = Search()
-    
+
     # Locking process
     pause.acquire()
-    
+
     # Getting data => a dictionary with structure: {tentGPS: value, planeGPS: value, angle: value} from input pipeline
-    values = pipelineIn.get()
-    
+    planeData = pipelineIn.get()
+    tentData =
     # Performing search using perform_search() of class Search
     search_result = search.perform_search(values["tentGPS"], values["planeGPS"], values["angle"])
-    
+
     # Putting data => a float value: search_result to output pipeline
     pipelineOut.put(search_result)
-    
+
     # Releasing lock
     pause.release()
