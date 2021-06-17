4de2e199696e54c19d555c36171ff301c59ac7dc
Xierumeng <xierumeng@hotmail.com>
2021-06-17 18:57:32 -0400

    A

--- modules/decklinksrc/decklinkSrcWorker.py
+++ modules/decklinksrc/decklinkSrcWorker.py
@@ -5,9 +5,17 @@ def decklinkSrcWorker(pause, exitRequest, pipelineOut):
     print("Start decklinksrc")
     decklinkSrc = DeckLinkSRC()
 
+    # i = 0  # Debugging
     while True:
+        # Debugging
+        # i += 1
+        # if i > 300:
+        #     decklinkSrc.stop()
+        #     return
+
         # Kill process if exit is requested
         if not exitRequest.empty():
+            decklinkSrc.stop()
             return
 
         pause.acquire()
@@ -16,6 +24,10 @@ def decklinkSrcWorker(pause, exitRequest, pipelineOut):
         curr_frame = decklinkSrc.grab()
         if curr_frame is None:
             continue
+
+        # Debugging
+        # cv2.imshow('VideoStream', curr_frame)
+        # cv2.waitKey(1)
 
         pipelineOut.put(curr_frame)
 
--- modules/decklinksrc/decklinksrc.py
+++ modules/decklinksrc/decklinksrc.py
@@ -36,6 +36,14 @@ class DeckLinkSRC:
         Initializes DeckLink video stream
         """
         self.__currentFrame = None
+        self.capture = None
+        self.start()
+
+    def start(self):
+        """
+        Logic for (re)starting video stream
+        """
+        self.__currentFrame = None
         self.capture = cv2.VideoCapture(0)  # Starts capture on initialization of object
 
     def stop(self):
@@ -49,12 +57,13 @@ class DeckLinkSRC:
         """
         Logic for grabbing single frame from DeckLink
         """
-        self.__currentFrame = self.capture.read()
+        success, self.__currentFrame = self.capture.read()
         return self.__currentFrame
 
     def recordVideo(self, filename, xdim, ydim):
         """
         Logic for 
+        UNTESTED, use OBS to record
 
         Parameters
         ----------
