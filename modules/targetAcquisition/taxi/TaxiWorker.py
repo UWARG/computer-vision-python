2da12d40b21470014a40d941683638d4ea8a8e5d
Atharva Gangal <70596721+thrvg@users.noreply.github.com>
2021-06-18 15:08:51 -0400

    Update TaxiWorker.py

--- modules/targetAcquisition/taxi/TaxiWorker.py
+++ modules/targetAcquisition/taxi/TaxiWorker.py
@@ -1,6 +1,27 @@
 from modules.targetAcquisition.taxi import Taxi
 
 def taxi_worker(pause, exitRequest, pipelineIn, pipelineOut):
+    """
+        Taxi Worker Process 
+        Implements multiprocessing for the taxi module
+        Gets frame from the input pipeline, performs target acquisition and puts a stop or move command into the output pipeline
+        
+        Parameters
+        ----------
+        pause : multiprocessing.Lock
+          a lock shared between worker processes
+        exitRequest : multiprocessing.Queue
+          a queue that determines when the worker process stops
+        pipelineIn : multiprocessing.Queue
+          input pipeline
+        pipelineOut : multiprocessing.Queue
+          output pipeline
+        
+        Returns
+        -------
+        None
+        """
+    
     print("Start Taxi")
     taxi = Taxi()
 
@@ -9,9 +30,12 @@ def taxi_worker(pause, exitRequest, pipelineIn, pipelineOut):
         pause.release()
         
         frame = pipelineIn.get()
-        taxi.main(frame)
-
-        pipelineOut.put()
+        
+        # Performing target acquisition using main() method of Taxi.py
+        # A stop/move command is returned which is put into the output pipeline
+        movement_command = taxi.main(frame)
+        
+        pipelineOut.put(movement_command)
 
         if not exitRequest.empty():
             return
