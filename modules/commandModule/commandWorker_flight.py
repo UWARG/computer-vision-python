7aff80212195015a2d0ed26390035ee3f046ac8a
Gary Xin <gary.xin19@gmail.com>
2021-06-17 17:09:07 -0700

    Caching pylon coordinates (#61)
    * Fixed indentation issues
    
    * Cached gps coordinates in a jank way
    
    * Fixed the names

--- modules/commandModule/commandWorker_flight.py
+++ modules/commandModule/commandWorker_flight.py
@@ -1,8 +1,10 @@
+import json
+
 from modules.commandModule.commandModule import CommandModule
 
 def flight_command_worker(pipelineIn, pipelineOut, pigo_dir=""):
 	command = CommandModule(pigoFileDirectory=pigo_dir)
-	while:
+	while True:
 		# pipelineIn gives [[x,y], [range]]
 		if pipelineIn.empty:
 		   continue
@@ -10,9 +12,12 @@ def flight_command_worker(pipelineIn, pipelineOut, pigo_dir=""):
 		data = pipelineIn.get()
 		gps_coordinates = data[0]
 
-		# Cache gps_coordinates here
+		# Cache gps_coordinates into a JSON file
+		with open("temp_pylon_gps", "w") as pylon_gps_file:
+			json.dump(gps_coordinates, pylon_gps_file)
 		
 		command.set_gps_coordinates(gps_coordinates)
+		return
 
 	#TODO: Write logic to get POGI
 
--- modules/search/searchWorker.py
+++ modules/search/searchWorker.py
@@ -1,11 +1,11 @@
 from modules.search.Search import Search
+import json
 
-
-def searchWorker(pause, exitRequest, pipelineIn, pipelineOut):
-     """
+def search_worker(pause, exitRequest, pipelineIn, pipelineOut):
+    """
         Search worker process to perform search with multiprocessing
         Gets data from the input pipeline, performs search, and puts it into the output pipeline
-        
+
         Parameters
         ----------
         pause : multiprocessing.Lock
@@ -16,27 +16,28 @@ def searchWorker(pause, exitRequest, pipelineIn, pipelineOut):
           input pipeline
         pipelineOut : multiprocessing.Queue
           output pipeline
-        
+
         Returns
         -------
         None
-        """
-  
+    """
     print("Start Search")
-    
+
     search = Search()
-    
+
     # Locking process
     pause.acquire()
-    
+
     # Getting data => a dictionary with structure: {tentGPS: value, planeGPS: value, angle: value} from input pipeline
-    values = pipelineIn.get()
-    
+    plane_data = pipelineIn.get()
+    with open("temp_pylon_gps", "r") as pylon_gps_file:
+        pylon_gps = json.load(pylon_gps_file)
+
     # Performing search using perform_search() of class Search
-    search_result = search.perform_search(values["tentGPS"], values["planeGPS"], values["angle"])
-    
+    search_result = search.perform_search(pylon_gps, plane_data['planeGPS'], plane_data['angle'])
+
     # Putting data => a float value: search_result to output pipeline
     pipelineOut.put(search_result)
-    
+
     # Releasing lock
     pause.release()
