fed7b10e91612e9eb66549a1367dd6dbed18f4e1
shrinjay <shrinjaymukherjee@gmail.com>
2021-06-21 02:08:01 -0600

    Updated searchWorker with mock entry for heading

--- modules/search/searchWorker.py
+++ modules/search/searchWorker.py
@@ -2,7 +2,7 @@ from modules.search.Search import Search
 import json
 import logging
 
-def searchWorker(pause, exitRequest, pipelineIn, pipelineOut):
+def searchWorker(plane_data):
     """
     Search worker process to perform search with multiprocessing
     Gets data from the input pipeline, performs search, and puts it into the output pipeline
@@ -28,18 +28,18 @@ def searchWorker(pause, exitRequest, pipelineIn, pipelineOut):
     
     search = Search()
     
-    pause.acquire()
-    pause.release()
-    
     # Getting data => a dictionary with structure: {tentGPS: value, planeGPS: value, angle: value} from input pipeline
-    plane_data = pipelineIn.get()
     with open("temp_pylon_gps", "r") as pylon_gps_file:
         pylon_gps = json.load(pylon_gps_file)
 
     # Performing search using perform_search() of class Search
-    search_result = search.perform_search(pylon_gps, plane_data['planeGPS'], plane_data['angle'])
-
-    # Putting data => a float value: search_result to output pipeline
-    pipelineOut.put(search_result)
+    plane_gps = {
+        "lat": plane_data["gpsCoordinates"]["lattitude"],
+        "lon": plane_data["gpsCoordinates"]["longtitude"]
+    }
+    search_result = search.perform_search(pylon_gps, plane_gps, plane_data['heading'])
     
     logger.debug("searchWorker: Stop Search Module")
+
+    # Putting data => a float value: search_result to output pipeline
+    return search_result
