5a19414ad1cd91e4b8003ff6fc741561c1ad5bef
shrinjay <shrinjaymukherjee@gmail.com>
2021-06-21 20:16:11 -0600

    corrected haversine implementation, wrote tests for search, fixed worker

--- modules/commandModule/commandWorker_taxi_first.py
+++ modules/commandModule/commandWorker_taxi_first.py
@@ -1,25 +1,27 @@
 from modules.commandModule.commandModule import CommandModule
 import logging
+from directories import PIGO_DIR, POGI_DIR
+
 
 def taxi_command_worker_first(pipelineIn, pipelineOut):
-	"""
-	Worker process that gets a turn command [dict] from the search worker and updates the pigo by calling set_ground_command()
-	
-	Parameters
-	----------
-	pipelineIn : multiprocessing.Queue
-	input pipeline that has a turn command dictionary with format {"heading": bearing, "latestDistance": 0}
-	pipelineOut : multiprocessing.Queue
-	output pipeline
+    """
+    Worker process that gets a turn command [dict] from the search worker and updates the pigo by calling set_ground_command()
+
+    Parameters
+    ----------
+    pipelineIn : multiprocessing.Queue
+    input pipeline that has a turn command dictionary with format {"heading": bearing, "latestDistance": 0}
+    pipelineOut : multiprocessing.Queue
+    output pipeline
     
-	Returns
-	-------
-	None
-	"""
-	
-	if pipelineIn.empty():
-		print("No data in taxi_command_worker_first pipelineIn")
-		return
-	
-	turn_command_data = pipelineIn.get()
-	set_ground_commands(turn_command_data)
+    Returns
+    -------
+    None
+    """
+
+    if pipelineIn.empty():
+        print("No data in taxi_command_worker_first pipelineIn")
+        return
+    command = CommandModule(pigoFileDirectory=PIGO_DIR, pogiFileDirectory=POGI_DIR)
+    turn_command_data = pipelineIn.get()
+    command.set_ground_commands(turn_command_data)
--- modules/search/searchWorker.py
+++ modules/search/searchWorker.py
@@ -2,42 +2,32 @@ from modules.search.Search import Search
 import json
 import logging
 
-def searchWorker(plane_data):
+def searchWorker(plane_data, pylon_gps):
     """
-    Search worker process to perform search with multiprocessing
-    Gets data from the input pipeline, performs search, and puts it into the output pipeline
+    Search worker process to perform search
+    Gets data from the input, performs search, and returns
     
     Parameters
     ----------
-    pause : multiprocessing.Lock
-        a lock shared between worker processes
-    exitRequest : multiprocessing.Queue
-        a queue that determines when the worker process stops
-    pipelineIn : multiprocessing.Queue
-        input pipeline
-    pipelineOut : multiprocessing.Queue
-        output pipeline
+    plane_data: POGI object
+    pylon_gps: dictionary containing last target gps coordinates, get from "temp_pylon_gps.json"
     
     Returns
     -------
-    None
+    serach_result: Ground command object
     """
   
     logger = logging.getLogger()
     logger.debug("searchWorker: Start Search Module")
     
     search = Search()
-    
-    # Getting data => a dictionary with structure: {tentGPS: value, planeGPS: value, angle: value} from input pipeline
-    with open("temp_pylon_gps", "r") as pylon_gps_file:
-        pylon_gps = json.load(pylon_gps_file)
 
     # Performing search using perform_search() of class Search
     plane_gps = {
-        "lat": plane_data["gpsCoordinates"]["lattitude"],
-        "lon": plane_data["gpsCoordinates"]["longtitude"]
+        "lattitude": plane_data["gpsCoordinates"]["lattitude"],
+        "longtitude": plane_data["gpsCoordinates"]["longtitude"]
     }
-    search_result = search.perform_search(pylon_gps, plane_gps, plane_data['heading'])
+    search_result = search.perform_search(pylon_gps, plane_gps)
     
     logger.debug("searchWorker: Stop Search Module")
 
