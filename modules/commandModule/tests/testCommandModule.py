4b6d0213a10e1022a2bc98a1e3ad2dd0d5f86a2e
Kevin Ma <kevinma@Kevins-MacBook-Pro.local>
2021-08-16 18:22:03 -0400

    modified mock command module

--- modules/commandModule/tests/testCommandModule.py
+++ modules/commandModule/tests/testCommandModule.py
@@ -30,10 +30,10 @@ class mockCommandModule:
     def __init__(self, pogiFileDirectory: str):
         self.pogiFileDirectory = pogiFileDirectory
         self.__pogiData = dict()
-        self.__pogiData['gpsCoordinates'] = [random.uniform(-90, 90), random.uniform(-180, 180), random.uniform(0, 100)]
+        self.__pogiData['gpsCoordinates'] = {'latitude': random.uniform(-90, 90), 'longitude': random.uniform(-180, 180), 'altitude': random.uniform(0, 100)}
         self.__pogiData['currentAirspeed'] = [round(random.uniform(0, 50), 7)]
-        self.__pogiData['eulerAnglesOfPlane'] = [random.uniform(0, math.pi * 2), random.uniform(0, math.pi * 2), random.uniform(0, math.pi * 2)]
-        self.__pogiData['eulerAnglesOfCamera'] = [random.uniform(0, math.pi * 2), random.uniform(0, math.pi * 2), random.uniform(0, math.pi * 2)]
+        self.__pogiData['eulerAnglesOfPlane'] = {'yaw': random.uniform(0, math.pi * 2), 'pitch': random.uniform(-math.pi/2, math.pi/2), 'roll': random.uniform(-math.pi/2, math.pi/2)}
+        self.__pogiData['eulerAnglesOfCamera'] = {'yaw': random.uniform(0, math.pi * 2), 'pitch': random.uniform(-math.pi/2, math.pi/2), 'roll': random.uniform(-math.pi/2, math.pi/2)}
         self.__pogiData['isLanded'] = [random.randint(0,1)]
 
     def write_to_json(self):
