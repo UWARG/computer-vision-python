07f8f83e1e5d6637c41b0b3654913608a31aa793
Xierumeng <xierumeng@hotmail.com>
2022-03-03 16:03:15 -0500

    Renaming directory

--- modules/TestIMUINterface/getIMUWorker.py
+++
@@ -1,8 +0,0 @@
-import time
-#from .getIMUData import getIMUINterface
-import getIMUData
-
-getIMUWorker = getIMUData.getIMUINterface('/dev/ttyACM0')
-
-while(True):
-    print(getIMUWorker.getIMUData()) 
