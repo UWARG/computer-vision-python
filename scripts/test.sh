afa722b09759e4e5f82cefd55ff2e80cecf21832
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2022-03-07 22:01:43 -0500

    try recursive init in bash script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,5 @@
 #bash
 
 pip install -r requirements.txt
-cd modules/targetAcquisition/Yolov5_DeepSort_Pytorch
-git submodule update --init
-cd ../../../
+git submodule update --init --recursive
 python test_main.py 
