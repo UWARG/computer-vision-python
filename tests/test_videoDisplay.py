6c799a552e6a12c4ea8424113c8158794641723e
Jerry Bi <jerrybi1202@outlook.com>
2022-04-07 11:33:22 -0400

    removed unittest references

--- tests/test_videoDisplay.py
+++ tests/test_videoDisplay.py
@@ -1,14 +1,16 @@
 import modules.videoDisplay.videoDisplay as vd
-import unittest
+import logging
+
+LOGGER = logging.getLogger()
 
 
-class TestVideoDisplay(unittest.TestCase):
-    def test_camera_display(self):
-        vd.displayCamera()
-        assert vd.is_open is False
+def test_camera_display():
+    vd.displayCamera()
+    assert vd.is_open is False
 
-    def test_log_output(self):
-        with self.assertLogs(level='DEBUG') as cm:
-            vd.displayVideo(None, None, None)
-            self.assertEqual(cm.output, ['DEBUG:root:videoDisplay: Started video display',
-                                         'DEBUG:root:videoDisplay: Stopped video display'])
+
+def test_log_output(caplog):
+    with caplog.at_level(logging.DEBUG):
+        vd.displayVideo(None, None, None)
+        assert 'Started video display' in caplog.text
+        assert 'Stopped video display' in caplog.text
