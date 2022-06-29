6cb292cdb54fa2958884511c00fcff886968a46d
aydanjiwani <a9jiwani@uwaterloo.ca>
2022-06-29 09:45:46 -0400

    initial commit, start recreating functions

---
+++ modules/targetAcquisition/personDetection/detectdependencies.py
@@ -0,0 +1,13 @@
+
+#from modules.targetAcquisition.Yolov5_DeepSort_Pytorch.yolov5.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_coords, 
+                                  #check_imshow, xyxy2xywh, xywh2xyxy, increment_path, set_logging)
+#from modules.targetAcquisition.Yolov5_DeepSort_Pytorch.yolov5.utils.torch_utils import select_device, time_sync
+#from modules.targetAcquisition.Yolov5_DeepSort_Pytorch.yolov5.utils.plots import Annotator, colors
+#from modules.targetAcquisition.Yolov5_DeepSort_Pytorch.deep_sort.utils.parser import get_config
+#from modules.targetAcquisition.Yolov5_DeepSort_Pytorch.yolov5.utils.augmentations import Albumentations, letterbox
+
+def check_img_size:
+
+def non_max_suppression:
+
+#add the rest
