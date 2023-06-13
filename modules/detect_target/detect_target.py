be0bfa0289064d8daa6a41973e22dc6652f2fb4d
Xierumeng <xierumeng@hotmail.com>
2023-06-13 01:06:35 -0400

    Logging bounding box initial

--- modules/detect_target/detect_target.py
+++ modules/detect_target/detect_target.py
@@ -1,12 +1,14 @@
 """
 Detects objects using the provided model
 """
+import time
 
 import cv2
 import numpy as np  # TODO: Remove
 import ultralytics
 
 from .. import frame_and_time
+from .. import detections_and_time
 
 
 # This is just an interface
@@ -19,7 +21,7 @@ class DetectTarget:
     def __init__(self, model_path: str, save_name: str=""):
         self.__model = ultralytics.YOLO(model_path)
         self.__counter = 0
-        self.__filename_prefix = save_name
+        self.__filename_prefix = save_name + "_" + str(int(time.time())) + "_"
 
     def run(self, data: frame_and_time.FrameAndTime) -> "tuple[bool, np.ndarray | None]":
         """
@@ -38,8 +40,23 @@ class DetectTarget:
             return False, None
 
         # TODO: Change this to image points for image and telemetry merge for 2024
-        # (bounding box conversion code required)
         image_annotated = predictions[0].plot(conf=True)
+
+        # Processing object detection
+        boxes = predictions[0].boxes
+        if boxes.shape[0] == 0:
+            return False, None
+
+        objects_bounds = boxes.xyxy.detach().cpu().numpy()
+        detections = detections_and_time.DetectionsAndTime(data.timestamp)
+        for i in range(0, boxes.shape[0]):
+            bounds = objects_bounds[i]
+            label = int(boxes.cls[i])
+            confidence = float(boxes.conf[i])
+            detection = detections_and_time.Detection(bounds, label, confidence)
+            detections.append(detection)
+
+        print(detections)
 
         # Logging
         if self.__filename_prefix != "":
