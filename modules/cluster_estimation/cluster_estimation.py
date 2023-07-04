b991a28a8a050f359ec608f40dccd26182cde8c5
YangMR <a76yang@uwaterloo.ca>
2023-07-03 23:50:36 -0400

    partial code import

--- modules/cluster_estimation/cluster_estimation.py
+++ modules/cluster_estimation/cluster_estimation.py
@@ -1,7 +1,12 @@
 """
 TODO: Write description
 """
-from position_object import PositionObject
+
+import sklearn
+import numpy as np
+# Parent directory import not working
+from object_in_world import ObjectInWorld
+
 # Placeholder:
 from detection_in_world import DetectionInWorld
 
@@ -9,14 +14,32 @@ class ClusterEstimation:
     """
     TODO: Write description
     """
-    def __init__(self):
-        # TODO: Settings etc.
-        raise NotImplementedError
+    def __init__(self, co_type="spherical", components=10, rand_state=0, weight_conc_prior=100, mean_precision_prior=1E-6,
+                 min_points=100, new_points_per_run=10):
+        # Initalizes VMGMM model 
+        self.__vgmm_model = sklearn.mixture.BayesianGaussianMixture(
+            covariance_type = co_type, 
+            n_components = components, 
+            random_state = rand_state, 
+            weight_concentration_prior = weight_conc_prior, 
+            init_params = 'k-means++', 
+            mean_precision_prior = mean_precision_prior)
+        # Points storage 
+        self.__all_points = []
+        self.__min_points = min_points
+        self.__points_per_run = new_points_per_run
+        self.__no_points_count = 0
+        self.__current_bucket = []
         
-    def run(self, detections: "list[DetectionInWorld]", run_override:  bool) -> "tuple[bool, list[PositionObject | None]]":
+    def run(self, detections: "list[DetectionInWorld]", run_override:  bool) -> "tuple[bool, list[ObjectInWorld | None]]":
         """
-        TODO: Write description
+        Take in list of landing pad detections and return list of estimated landing pad locations
+        if number of detections is sufficient, or if manually forced to run
         """
+
+        Z = self.__vgmm_model.predict(detections)
+        print(Z)
+
         if not run_override and not self.decide_to_run(detections):
             return False, None
 
@@ -30,3 +53,9 @@ class ClusterEstimation:
             return True
         else:
             return False
+
+
+# Debug
+simulated_detections, y = sklearn.datasets.make_blobs(n_samples=150, centers=4, n_features=2, cluster_std=0.1, center_box=(0,500))
+clusest = ClusterEstimation(simulated_detections, False)
+clusest.run()
--- modules/cluster_estimation/position_object.py
+++
@@ -1,5 +0,0 @@
-class PositionObject:
-    def __init__(self, x, y, covariance):
-        self.location_x = x
-        self.location_y = y
-        self.spherical_variance = covariance
