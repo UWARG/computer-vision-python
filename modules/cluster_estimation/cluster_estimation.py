8755471dfc3d605b624dcb5d780f49ee5a5f13a6
YangMR <a76yang@uwaterloo.ca>
2023-07-05 00:50:12 -0400

    bug fixes + testing + return highest valid means

--- modules/cluster_estimation/cluster_estimation.py
+++ modules/cluster_estimation/cluster_estimation.py
@@ -6,12 +6,14 @@ drop of its mean compared to the next greatest mean.
 """
 
 import sklearn
+import sklearn.datasets
+import sklearn.mixture
 import numpy as np
 # Parent directory import not working
-from modules.object_in_world import ObjectInWorld
+from object_in_world import ObjectInWorld
 
 # Placeholder:
-from modules.cluster_estimation.detection_in_world import DetectionInWorld
+from detection_in_world import DetectionInWorld
  
 class ClusterEstimation:
     """
@@ -50,6 +52,8 @@ class ClusterEstimation:
         weights = self.__vgmm_model.weights_
 
 
+        print(weights)
+
         i = 0
         while i < len(clusters):
             nearby_point = False
@@ -67,18 +71,30 @@ class ClusterEstimation:
                 i += 1
 
 
-
+        print(weights)
         indices = np.argsort(weights)[::-1]
         weights = np.sort(weights)[::-1]
-
+        print(weights)
+        print(indices)
+        print(clusters)
 
         # Loop through each cluster
         # clusters is a list of centers ordered by weights
         # most likely cluster listed first in descending weights order
-        total_clusters = clusters.shape[0]
-        num_viable_clusters = 1
+        
 
         objects_in_world = []
+        # Return, at the very least, the cluster with the highest mean
+        object_created, object_to_add = ObjectInWorld.create(
+            clusters[indices[0]][0], 
+            clusters[indices[0]][1], 
+            covariances[indices[0]]
+            )
+        if object_created:
+            objects_in_world.append(object_to_add)
+        
+        total_clusters = clusters.shape[0]
+        num_viable_clusters = 1
         # Drop all clusters after a 50% drop in weight occurs
         while num_viable_clusters < total_clusters:
             if (weights[num_viable_clusters] / weights[num_viable_clusters-1]) > 2:
@@ -86,14 +102,13 @@ class ClusterEstimation:
             # Append new ObjectInWorld to output list
             # TODO: Verify if current indexing is correct 
             object_created, object_to_add = ObjectInWorld.create(
-                clusters[indices[num_viable_clusters]][0].items(), 
-                clusters[indices[num_viable_clusters]][1].items(), 
+                clusters[indices[num_viable_clusters]][0], 
+                clusters[indices[num_viable_clusters]][1], 
                 covariances[indices[num_viable_clusters]]
                 )
             if object_created:
                 objects_in_world.append(object_to_add)
             num_viable_clusters += 1
-        # TODO: Decide when to not return any cluster centers
         return True, objects_in_world
 
     def decide_to_run(self, detections: "list[DetectionInWorld]") -> bool:
@@ -105,19 +120,23 @@ class ClusterEstimation:
         if len(self.__all_points) + len(self.__current_bucket) < self.__min_points:
             return False
         # Don't run if not enough new points
-        if self.__current_bucket < self.__points_per_run:
+        if len(self.__current_bucket) < self.__points_per_run:
             return False
         # Both requirements met, empty bucket and run
         self.__all_points += self.__current_bucket
         self.__current_bucket = 0
         return True
         
-    def get_distance(cluster, point) -> float:
+    def get_distance(self, cluster, point) -> float:
         return np.sqrt(np.square(cluster[0]-point[0])+np.square(cluster[1]-point[1]))
 
 
 # Debug
 if __name__ == "__main__":
     simulated_detections, y = sklearn.datasets.make_blobs(n_samples=150, centers=4, n_features=2, cluster_std=0.1, center_box=(0,500))
-    clusest = ClusterEstimation(simulated_detections, False)
-    clusest.run()
+    clusest = ClusterEstimation()
+    ret_bool, ret_list = clusest.run(np.ndarray.tolist(simulated_detections), False)
+    print(ret_bool)
+    print(len(ret_list))
+    for i in ret_list:
+        print("x: %f, y: %f, var: %f" % (i.position_x, i.position_y, i.spherical_variance))
---
+++ modules/cluster_estimation/object_in_world.py
@@ -0,0 +1,34 @@
+"""
+Position of the object in world space
+"""
+
+
+# Basically a struct
+# pylint: disable=too-few-public-methods
+class ObjectInWorld:
+    """
+    Contains the estimated position of the object
+    """
+    __create_key = object()
+
+    @classmethod
+    def create(cls, position_x: float, position_y: float, spherical_variance: float) -> "tuple[bool, ObjectInWorld | None]":
+        """
+        Position in local coordinates
+        """
+        if spherical_variance < 0.0:
+            return False, None
+
+        return True, ObjectInWorld(cls.__create_key, position_x, position_y, spherical_variance)
+
+    def __init__(self, class_private_create_key, position_x: float, position_y: float, spherical_variance: float):
+        """
+        Private constructor, use create() method
+        """
+        assert class_private_create_key is ObjectInWorld.__create_key, "Use create() method"
+
+        self.position_x = position_x
+        self.position_y = position_y
+        self.spherical_variance = spherical_variance
+
+# pylint: enable=too-few-public-methods
