e09c84a6db46615c52569b5a9326b1baee68dfdc
hepromark <markdo28102004@gmail.com>
2023-08-25 13:48:03 -0400

    Update cluster_estimation.py

--- modules/cluster_estimation/cluster_estimation.py
+++ modules/cluster_estimation/cluster_estimation.py
@@ -29,12 +29,6 @@ class ClusterEstimation:
     rand_state: int, default = 0
         seed for randomizer, to get consistent results
     
-    __WEIGHT_CONC_PRIOR: int, default = 100
-        higher value 
-    
-    __MEAN_PRECISION_PRIOR: int, default = 1E-6
-        lower value
-    
     min_activation_threshold: int, default = 100
         minimum total data points before model can be ran 
 
@@ -56,14 +50,19 @@ class ClusterEstimation:
 
     __get_distance()
 
-    ___convert_detections_to_point()
+    __convert_detections_to_point()
+
+    __filter_by_points_ownership()
+    
+    __sort_by_weights()
     """
     # Model Hyperparams 
     __MODEL_INIT_PARAM = "k-means++"
     __COVAR_TYPE = "spherical"
-    __WEIGHT_CONC_PRIOR=100
-    __MEAN_PRECISION_PRIOR=1E-6
+    __WEIGHT_CONC_PRIOR = 100
+    __MEAN_PRECISION_PRIOR = 1E-6
     __WEIGHT_DROP_THRESHOLD = 0.9
+    __MAX_CLUSTER_SIZE = 2
 
     def __init__(self,
                  components=10,
@@ -104,7 +103,7 @@ class ClusterEstimation:
         self.__vgmm_model = sklearn.base.clone(self.__vgmm_model)
 
     # EXPERIMENTAL: USING PREDICTIONS TO CHECK VALID CLUSTERS
-    def _filter_by_points_ownership(self, weights, covariances, clusters):
+    def __filter_by_points_ownership(self, weights, covariances, clusters):
         results = self.__vgmm_model.predict(self.__all_points)
 
         # Filtering by each cluster's point ownership
@@ -124,8 +123,16 @@ class ClusterEstimation:
         print(clusters)
         print("-----------------------------------------------")
     
-    def _sort_by_weights(self, cluster_weight_covariances:tuple[np.ndarray, np.float64, np.float64]) -> tuple[np.ndarray, np.float64, np.float64]:
-        pass
+    def __sort_by_weights(self, cluster_weight_covariances:tuple[np.ndarray, np.float64, np.float64]) -> tuple[np.ndarray, np.float64, np.float64]:
+        # indices = np.argsort(weights)[::-1]
+        # weights = np.sort(weights)[::-1]
+        # temp_arr = np.array(covariances)
+        # for i in range(len(indices)):
+        #     covariances[i] = temp_arr[indices[i]]
+        # temp_arr = np.array(clusters)
+        # for i in range(len(indices)):
+        #     clusters[i] = temp_arr[indices[i]]
+        raise NotImplementedError
 
     def run(self, detections: "list[DetectionInWorld]", run_override: bool) -> "tuple[bool, list[ObjectInWorld | None]]":
         """
@@ -149,6 +156,8 @@ class ClusterEstimation:
             list containing ObjectInWorld objects, containing position and covariance value. None if conditions not met
             and model not ran
         """
+        # Store new input data
+        self.__current_bucket += self.__convert_detections_to_point(detections)
 
         # Decide to run
         if not run_override and not self._decide_to_run(detections):
@@ -160,8 +169,8 @@ class ClusterEstimation:
         clusters = self.__vgmm_model.means_
         covariances = self.__vgmm_model.covariances_
         weights = self.__vgmm_model.weights_
-        # make tuple
-        clusters_weights_covariances = zip(clusters, weights, covariances)
+
+        model_output = list(zip(clusters, weights, covariances))
 
         print("Initial:")
         print(weights)
@@ -170,45 +179,32 @@ class ClusterEstimation:
         print("-----------------------------------------------")
 
         # Sort weights from largest to smallest, along with corresponding covariances and means
+        # Tuple comes sorted ONLY IF MODEL CONVERGES
         if not converged:
-            self._sort_by_weights()
-        # indices = np.argsort(weights)[::-1]
-        # weights = np.sort(weights)[::-1]
-        # temp_arr = np.array(covariances)
-        # for i in range(len(indices)):
-        #     covariances[i] = temp_arr[indices[i]]
-        # temp_arr = np.array(clusters)
-        # for i in range(len(indices)):
-        #     clusters[i] = temp_arr[indices[i]]
-
+            self.__sort_by_weights()
+        
         print("Sorted:")
         print(weights)
         print(covariances)
         print(clusters)
         print("-----------------------------------------------")
 
-        # Loop through each cluster
-        # clusters is a list of centers ordered by weights
-        # most likely cluster listed first in descending weights order
-        objects_in_world = []
+        # Loop through each cluster and drop all clusters after a __WEIGHT_DROP_THRESHOLD drop in weight occured
         num_viable_clusters = 1
-        # Drop all clusters after a __WEIGHT_DROP_THRESHOLD drop in weight occured
+        weight_drop_thresh_reached = False
         while num_viable_clusters < clusters.shape[0] and not weight_drop_thresh_reached:
-            weight_drop_thresh_reached = False
-
-            if (weights[num_viable_clusters-1] / weights[num_viable_clusters]) > (1 / self.__WEIGHT_DROP_THRESHOLD):
-                weights = weights[:num_viable_clusters]
-                clusters = clusters[:num_viable_clusters]
-                covariances = covariances[:num_viable_clusters]
+            # Check if weight dropped occured between previous cluster and current cluster
+            if (model_output[1][num_viable_clusters-1] / model_output[1][num_viable_clusters]) > (1 / self.__WEIGHT_DROP_THRESHOLD):
+                model_output = model_output[:num_viable_clusters]
                 weight_drop_thresh_reached = True
 
             num_viable_clusters += 1
 
-        # print("Weight-filtered:")
-        # print(weights)
-        # print(covariances)
-        # print(clusters)
-        # print("-----------------------------------------------")
+        print("Weight-filtered:")
+        print(weights)
+        print(covariances)
+        print(clusters)
+        print("-----------------------------------------------")
 
         # Bad detection removal
         i = 0
@@ -216,8 +212,8 @@ class ClusterEstimation:
             nearby_point = False
             j = 0
             while j < len(self.__all_points):
-                # Remove cluster if nothing within 2 metres
-                if self.__get_distance(clusters[i], self.__all_points[j]) < 10:
+                # Remove cluster if nothing within __MAX_CLUSTER_SIZE metres
+                if self.__get_distance(clusters[i], self.__all_points[j]) < self.__MAX_CLUSTER_SIZE:
                     nearby_point = True
                     break
                 j += 1
@@ -227,40 +223,35 @@ class ClusterEstimation:
             else:
                 i += 1
 
-
         print("Bad clusters removed:")
         print(weights)
         print(covariances)
         print(clusters)
         print("-----------------------------------------------")
-        print()
-        print()
 
-        # Create output list of remaining (valid) clusters 
+        # Create output list of remaining valid clusters 
+        detections_in_world = []
         for i in range(len(clusters)):
-            object_created, object_to_add = ObjectInWorld.create(
-                clusters[i][0], 
-                clusters[i][1], 
-                covariances[i]
-                )
-            if object_created:
-                objects_in_world.append(object_to_add)
+            result_created, result_to_add = ObjectInWorld.create(clusters[i][0],
+                                                                 clusters[i][1],
+                                                                 covariances[i],)
+            if result_created:
+                detections_in_world.append(result_to_add)
 
+        return True, detections_in_world
 
-
-        return True, objects_in_world
-
-    def _decide_to_run(self, detections: "list[DetectionInWorld]") -> bool:
+    def __decide_to_run(self, detections: "list[DetectionInWorld]") -> bool:
         """
         Decide when to run cluster estimation model 
         """
-        self.__current_bucket += self.___convert_detections_to_point(detections)
         # Don't run if total points under minimum requirement
         if len(self.__all_points) + len(self.__current_bucket) < self.__min_activation_threshold:
             return False
+        
         # Don't run if not enough new points
         if len(self.__current_bucket) < self.__points_per_run and self.__has_ran_once == True:
             return False
+        
         # Requirements met, empty bucket and run
         self.__all_points += self.__current_bucket
         self.__current_bucket = []
@@ -268,19 +259,18 @@ class ClusterEstimation:
 
         return True
         
-    def _get_distance(self, cluster, point) -> float:
+    def __get_distance(self, cluster, point) -> float:
         """
         Get distance between a point and a cluster center
         """
-        return np.sqrt(np.square(cluster[0]-point[0])+np.square(cluster[1]-point[1]))
+        return np.linalg.norm(cluster - point)
 
-    def ___convert_detections_to_point(self, detections: "list[DetectionInWorld]") -> list[float, float]:
+    def __convert_detections_to_point(self, detections: "list[DetectionInWorld]") -> list[tuple(float, float)]:
         """
         Convert DetectionInWorld input object to a [x,y] position to store 
         """
         points = []
         for detection in detections:
-            points.append([detection.location_x, detection.location_y])
+            points.append((detection.location_x, detection.location_y))
 
         return points
-
