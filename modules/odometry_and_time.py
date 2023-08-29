dfee2bd66d339e87c3f5c311df6e91c9c0137e2f
ArVMal <87793211+ArVMal@users.noreply.github.com>
2023-08-29 06:52:18 -0400

    Update odometry_and_time.py

--- modules/odometry_and_time.py
+++ modules/odometry_and_time.py
@@ -1,5 +1,9 @@
 """
+<<<<<<< HEAD
 Drone odometry in local space and timestamp.
+=======
+Drone odometry and timestamp
+>>>>>>> 78c49d9 (Update odometry_and_time.py)
 """
 import time
 
@@ -10,11 +14,12 @@ from . import drone_odometry_local
 # pylint: disable=too-few-public-methods
 class OdometryAndTime:
     """
-    Contains odometry/telemetry and timestamp.
+    Contains odometry/telemetry and timestamp
     """
     __create_key = object()
 
     @classmethod
+<<<<<<< HEAD
     def create(cls, odometry_data: drone_odometry_local.DroneOdometryLocal) \
         -> "tuple[bool, OdometryAndTime | None]":
         """
@@ -39,5 +44,30 @@ class OdometryAndTime:
 
         self.odometry_data = odometry_data
         self.timestamp = timestamp
+=======
+    def create(cls, 
+               position: drone_odometry.DronePosition, 
+               orientation: drone_odometry.DroneOrientation) -> "tuple[bool, MergedOdometryDetections | None]":
+        """
+        position: Latitude, longitude in decimal degrees and altitude in metres
+        orientation: Yaw, pitch, roll following NED system (x forward, y right, z down)
+        """
+
+        return True, OdometryAndTime(cls.__create_key, position, orientation)
+        
+    def __init__(self,
+                 class_private_create_key,
+                 position: drone_odometry.DronePosition,
+                 orientation: drone_odometry.DroneOrientation):
+        """
+        Private constructor, use create() method
+        Constructor sets timestamp to current time
+        """
+        assert class_private_create_key is OdometryAndTime.__create_key, "Use create() method"
+                     
+        self.position = position
+        self.orientation = orientation
+        self.timestamp = time.time()
+>>>>>>> 78c49d9 (Update odometry_and_time.py)
 
 # pylint: enable=too-few-public-methods
