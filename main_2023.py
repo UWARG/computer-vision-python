d8a9c07afe5eafbb266732fdc2cc87c67dd17df3
TongguangZhang <tonggz250@gmail.com>
2023-09-18 14:31:05 -0400

    one config file, option to force cpu

--- main_2023.py
+++ main_2023.py
@@ -13,28 +13,28 @@ from utilities.workers import worker_manager
 from modules.detect_target import detect_target_worker
 from modules.video_input import video_input_worker
 
-# select config file from command line
-parser = argparse.ArgumentParser()
-parser.add_argument("--config", choices=["cuda", "cpu"], required=True, help="Select configuration file")
-args = parser.parse_args()
-config_file = "config.yaml" if args.config == "cuda" else "config_no_cuda.yaml"
-with open(config_file, 'r', encoding="utf8") as file:
-    config = yaml.safe_load(file)
-
-
-QUEUE_MAX_SIZE = config["queue_max_size"]
-
-VIDEO_INPUT_CAMERA_NAME = config["video_input"]["camera_name"]
-VIDEO_INPUT_WORKER_PERIOD = config["video_input"]["worker_period"]
-VIDEO_INPUT_SAVE_PREFIX = config["video_input"]["save_prefix"]
-
-DETECT_TARGET_WORKER_COUNT = config["detect_target"]["worker_count"]
-DETECT_TARGET_DEVICE = config["detect_target"]["device"]
-DETECT_TARGET_MODEL_PATH = config["detect_target"]["model_path"]        # TODO: update in config files
-DETECT_TARGET_SAVE_PREFIX = config["detect_target"]["save_prefix"]
-
-
 if __name__ == "__main__":
+
+    # open command file
+    with open("config.yaml", 'r', encoding="utf8") as file:
+        config = yaml.safe_load(file)
+
+    # parse whether or not to force cpu from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cpu", action="store_true", help="option to force cpu")
+    args = parser.parse_args()
+
+    QUEUE_MAX_SIZE = config["queue_max_size"]
+
+    VIDEO_INPUT_CAMERA_NAME = config["video_input"]["camera_name"]
+    VIDEO_INPUT_WORKER_PERIOD = config["video_input"]["worker_period"]
+    VIDEO_INPUT_SAVE_PREFIX = config["video_input"]["save_prefix"]
+
+    DETECT_TARGET_WORKER_COUNT = config["detect_target"]["worker_count"]
+    DETECT_TARGET_DEVICE =  "cpu" if args.cpu else config["detect_target"]["device"]
+    DETECT_TARGET_MODEL_PATH = config["detect_target"]["model_path"]   # TODO: update in config file
+    DETECT_TARGET_SAVE_PREFIX = config["detect_target"]["save_prefix"]
+
     # Setup
     controller = worker_controller.WorkerController()
 
