b42187b683a588fba0c07cb58e4cac099bdea77b
Ethan Woo <108487936+ethancpwoo@users.noreply.github.com>
2023-09-27 23:20:28 -0400

    Flight input worker (#145)
    * added flight input worker
    
    * added TODO
    
    * added create to flight_interface and renamed files
    
    * fixed create issues and main 2023 modifications
    
    * changed constructors and create() methods
    
    * fixed create method
    
    * fixed hint

---
+++ modules/flight_input/flight_interface.py
@@ -0,0 +1,48 @@
+"""
+Creates flight controller and combines odometry data and timestamp.
+"""
+
+from modules.common.mavlink.modules import flight_controller
+from modules import odometry_and_time
+
+
+# This is just an interface
+# pylint: disable=too-few-public-methods
+class FlightInterface:
+    """
+    Create flight controller and combines odometry data and timestamp.
+    """
+    __create_key = object()
+
+    @classmethod
+    def create(cls, address: str) -> "tuple[bool, FlightInterface | None]":
+        """
+        address: TCP or port.
+        """
+        result, controller = flight_controller.FlightController.create(address)
+
+        if not result:
+            return False, None
+
+        return True, FlightInterface(cls.__create_key, controller)
+
+    def __init__(self, class_private_create_key, controller: flight_controller.FlightController):
+        """
+        Private constructor, use create() method.
+        """
+        assert class_private_create_key is FlightInterface.__create_key, "Use create() method"
+
+        self.controller = controller
+
+    def run(self) -> "tuple[bool, odometry_and_time.OdometryAndTime]":
+        """
+        Returns a possible OdometryAndTime with current timestamp.
+        """
+        result, data = self.controller.get_odometry()
+
+        if not result:
+            return False, None
+
+        return odometry_and_time.OdometryAndTime.create(data)
+
+# pylint: enable=too-few-public-methods
---
+++ modules/flight_input/flight_interface_worker.py
@@ -0,0 +1,37 @@
+"""
+Gets odometry information from drone.
+"""
+import time
+
+from utilities.workers import queue_proxy_wrapper
+from utilities.workers import worker_controller
+from . import flight_interface
+
+
+def flight_input_worker(address: str,
+                        period: float,
+                        output_queue: queue_proxy_wrapper.QueueProxyWrapper,
+                        controller: worker_controller.WorkerController):
+    """
+    Worker process. 
+
+    address is initial setting.
+    period is minimum period between loops.
+    output_queue is the data queue.
+    controller is how the main process communicates to this worker process.
+    """
+    result, interface = flight_interface.FlightInterface(address)
+
+    if not result:
+        return
+
+    while not controller.is_exit_requested():
+        controller.check_pause()
+
+        time.sleep(period)
+
+        result, value = interface.run()
+        if not result:
+            continue
+
+        output_queue.queue.put(value)
