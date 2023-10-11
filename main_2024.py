c4c525c28d0a50ffb41dab420a156dc567096a07
leo-cf-tian <69664426+leo-cf-tian@users.noreply.github.com>
2023-10-10 21:21:31 -0400

    Create main_2024.py (#153)
    * Create main_2024.py

--- main_2024.py
+++ main_2024.py
@@ -1,9 +1,10 @@
 """
-For 2022-2023 UAS competition.
+For 2023-2024 UAS competition.
 """
 import argparse
 import multiprocessing as mp
 import pathlib
+import queue
 
 import cv2
 import yaml
@@ -12,6 +13,7 @@ from utilities.workers import queue_proxy_wrapper
 from utilities.workers import worker_controller
 from utilities.workers import worker_manager
 from modules.detect_target import detect_target_worker
+from modules.flight_interface import flight_interface_worker
 from modules.video_input import video_input_worker
 
 
@@ -54,6 +56,9 @@ def main() -> int:
         DETECT_TARGET_DEVICE =  "cpu" if args.cpu else config["detect_target"]["device"]
         DETECT_TARGET_MODEL_PATH = config["detect_target"]["model_path"]
         DETECT_TARGET_SAVE_PREFIX = config["detect_target"]["save_prefix"]
+
+        FLIGHT_INTERFACE_ADDRESS = config["flight_interface"]["address"]
+        FLIGHT_INTERFACE_WORKER_PERIOD = config["flight_interface"]["worker_period"]
     except KeyError:
         print("Config key(s) not found")
         return -1
@@ -69,6 +74,10 @@ def main() -> int:
     detect_target_to_main_queue = queue_proxy_wrapper.QueueProxyWrapper(
         mp_manager,
         QUEUE_MAX_SIZE,
+    )
+    flight_interface_to_main_queue = queue_proxy_wrapper.QueueProxyWrapper(
+        mp_manager,
+        QUEUE_MAX_SIZE
     )
 
     video_input_manager = worker_manager.WorkerManager()
@@ -98,16 +107,46 @@ def main() -> int:
         ),
     )
 
+    flight_interface_manager = worker_manager.WorkerManager()
+    flight_interface_manager.create_workers(
+        1,
+        flight_interface_worker.flight_interface_worker,
+        (
+            FLIGHT_INTERFACE_ADDRESS,
+            FLIGHT_INTERFACE_WORKER_PERIOD,
+            flight_interface_to_main_queue,
+            controller,
+        ),
+    )
+
     # Run
     video_input_manager.start_workers()
     detect_target_manager.start_workers()
+    flight_interface_manager.start_workers()
 
     while True:
-        image = detect_target_to_main_queue.queue.get()
+        try:
+            image = detect_target_to_main_queue.queue.get_nowait()
+        except queue.Empty:
+            image = None
+
+        odometry_and_time = flight_interface_to_main_queue.queue.get()
+
+        if odometry_and_time is not None:
+            print("timestamp: " + str(odometry_and_time.timestamp))
+            print("lat: " + str(odometry_and_time.odometry_data.position.latitude))
+            print("lon: " + str(odometry_and_time.odometry_data.position.longitude))
+            print("alt: " + str(odometry_and_time.odometry_data.position.altitude))
+            print("yaw: " + str(odometry_and_time.odometry_data.orientation.yaw))
+            print("roll: " + str(odometry_and_time.odometry_data.orientation.roll))
+            print("pitch: " + str(odometry_and_time.odometry_data.orientation.pitch))
+            print("")
+
         if image is None:
             continue
 
         cv2.imshow("Landing Pad Detector", image)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -116,9 +155,11 @@ def main() -> int:
 
     video_input_to_detect_target_queue.fill_and_drain_queue()
     detect_target_to_main_queue.fill_and_drain_queue()
+    flight_interface_to_main_queue.fill_and_drain_queue()
 
     video_input_manager.join_workers()
     detect_target_manager.join_workers()
+    flight_interface_manager.join_workers()
 
     return 0
 
