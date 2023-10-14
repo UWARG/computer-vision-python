d7e87e806e36e5aa93419a35bc040469cbaf3283
Tyler Chen <tyche4490@gmail.com>
2023-10-14 10:08:17 -0400

    Renamed and modified functions to match list from asana, adjusted data storage and parameters

--- modules/decision_command.py
+++ modules/decision_command.py
@@ -11,29 +11,31 @@ class Command:
         """
         Different types of commands.
         """
-        NULL                     = 0  # Stay the course
-        SET_RELATIVE_DESTINATION = 1  # Move relative to current position
-        HALT                     = 2  # Stop the drone ASAP
-        LAND                     = 3  # Land at current position
+        MOVE_RELATIVE    = 0  # Move relative to current position
+        MOVE_ABSOLUTE    = 1  # Move to absolute position
+        LAND_IMMEDIATE   = 2  # Stop the drone at current position
+        LAND_AT_POSITION = 3  # Stop the drone at position
 
     __create_key = object()
 
     @classmethod
-    def create_null_command(cls) -> "Command":
+    def create_relative_movement_command(cls,
+                                         relative_x: float,
+                                         relative_y: float) -> "Command":
         """
-        Command that has no effect (default).
+        Command for drone movement relative to current position.
         """
         return Command(
             cls.__create_key,
-            Command.CommandType.NULL,
-            0.0,
-            0.0,
+            Command.CommandType.MOVE_RELATIVE,
+            relative_x,
+            relative_y,
         )
 
     @classmethod
-    def create_set_relative_destination_command(cls,
-                                                relative_x: float,
-                                                relative_y: float) -> "Command":
+    def create_absolute_movement_command(cls,
+                                        absolute_x: float,
+                                        absolute_y: float) -> "Command":
         """
         Command to set drone destination.
         Drone must be in halted state.
@@ -41,49 +43,51 @@ class Command:
         """
         return Command(
             cls.__create_key,
-            Command.CommandType.SET_RELATIVE_DESTINATION,
-            relative_x,
-            relative_y,
+            Command.CommandType.MOVE_ABSOLUTE,
+            absolute_x,
+            absolute_y,
         )
 
     @classmethod
-    def create_halt_command(cls) -> "Command":
+    def create_land_immediate_command(cls) -> "Command":
         """
         Command to halt the drone.
         """
         return Command(
             cls.__create_key,
-            Command.CommandType.HALT,
+            Command.CommandType.LAND_IMMEDIATE,
             0.0,
             0.0,
         )
 
     @classmethod
-    def create_land_command(cls) -> "Command":
+    def create_land_at_position_command(cls, 
+                                        x: float, 
+                                        y: float) -> "Command":
         """
         Command to land the drone.
         Drone must be in halted state.
         """
         return Command(
             cls.__create_key,
-            Command.CommandType.LAND,
-            0.0,
-            0.0,
+            Command.CommandType.LAND_AT_POSITION,
+            x,
+            y,
         )
 
     def __init__(self,
                  class_private_create_key,
                  command_type: CommandType,
-                 relative_x: float,
-                 relative_y: float):
+                 command_x: float,
+                 command_y: float):
         """
         Private constructor, use create() method.
         """
         assert class_private_create_key is Command.__create_key, "Use create() method"
 
         self.__command_type = command_type
-        self.__relative_x = relative_x
-        self.__relative_y = relative_y
+        self.__command_x = command_x
+        self.__command_y = command_y
 
     def get_command_type(self) -> CommandType:
         """
@@ -91,11 +95,11 @@ class Command:
         """
         return self.__command_type
 
-    def get_relative_destination(self) -> "tuple[float, float]":
+    def get_command_position(self) -> "tuple[float, float]":
         """
         Getter.
         """
-        return self.__relative_x, self.__relative_y
+        return self.__command_x, self.__command_y
 
     def __repr__(self) -> str:
         """
@@ -103,7 +107,7 @@ class Command:
         """
         representation = "Command: " + str(self.__command_type)
 
-        if self.__command_type == Command.CommandType.SET_RELATIVE_DESTINATION:
-            representation += " " + str(self.get_relative_destination())
+        if self.__command_type != Command.CommandType.LAND_IMMEDIATE:
+            representation += " " + str(self.get_command_position())
 
         return representation
