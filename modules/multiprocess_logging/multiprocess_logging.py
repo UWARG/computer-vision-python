ef430c1c449d0a268acdabd0bbf9b2cd9f315b9c
TongguangZhang <tonggz250@gmail.com>
2024-03-23 22:40:16 -0400

    logging helper functions

--- modules/multiprocess_logging/multiprocess_logging.py
+++ modules/multiprocess_logging/multiprocess_logging.py
@@ -1,9 +1,18 @@
 import inspect
-import linecache
+import logging
+
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+WARNING = logging.WARNING
+ERROR = logging.ERROR
+CRITICAL = logging.CRITICAL
 
 def message_and_metadata(message, frame):
-    function_name = frame.f_code.co_name
-    filename = frame.f_code.co_filename
-    line_number = inspect.getframeinfo(frame).lineno
+        function_name = frame.f_code.co_name
+        filename = frame.f_code.co_filename
+        line_number = inspect.getframeinfo(frame).lineno
 
-    return f'[{filename} | {function_name} | {line_number}] {message}'
+        return f'[{filename} | {function_name} | {line_number}] {message}'
+
+def log_message(message, level, frame, queue):
+        queue.queue.put((message_and_metadata(message, frame), level), block=False)
