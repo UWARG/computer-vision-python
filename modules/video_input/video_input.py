63d49f9b282d8b8dc5a2e8ee1ade3b1b8ccf5b8c
TongguangZhang <tonggz250@gmail.com>
2024-03-24 15:24:10 -0400

    merge conflict in video input

--- modules/video_input/video_input.py
+++ modules/video_input/video_input.py
@@ -1,8 +1,12 @@
 """
 Combines image and timestamp together.
 """
+import inspect
+import queue
 
+from utilities.workers import queue_proxy_wrapper
 from ..common.camera.modules import camera_device
+from ..multiprocess_logging import multiprocess_logging
 from .. import image_and_time
 
 
@@ -11,14 +15,28 @@ class VideoInput:
     Combines image and timestamp together.
     """
 
-    def __init__(self, camera_name: "int | str", save_name: str = "") -> None:
+    def __init__(
+        self,
+        logging_queue: queue_proxy_wrapper.QueueProxyWrapper,
+        camera_name: "int | str",
+        save_name: str = ""
+    ) -> None:
         self.device = camera_device.CameraDevice(camera_name, 1, save_name)
+        self.logging_queue = logging_queue
 
     def run(self) -> "tuple[bool, image_and_time.ImageAndTime | None]":
         """
         Returns a possible ImageAndTime with current timestamp.
         """
         result, image = self.device.get_image()
+
+        try:
+            frame = inspect.currentframe()
+            multiprocess_logging.log_message(f'image size {image.shape}', multiprocess_logging.DEBUG, frame, self.logging_queue)
+            # self.logging_queue.queue.put((multiprocess_logging.message_and_metadata(f'image size {image.shape}', frame), logging.DEBUG), block=False)
+        except queue.Full:
+            pass
+
         if not result:
             return False, None
 
--- modules/video_input/video_input_worker.py
+++ modules/video_input/video_input_worker.py
@@ -1,11 +1,13 @@
 """
 Gets images from the camera.
 """
-
+import inspect
+import queue
 import time
 
 from utilities.workers import queue_proxy_wrapper
 from utilities.workers import worker_controller
+from ..multiprocess_logging import multiprocess_logging
 from . import video_input
 
 
@@ -14,6 +16,7 @@ def video_input_worker(
     period: float,
     save_name: str,
     output_queue: queue_proxy_wrapper.QueueProxyWrapper,
+    logging_queue: queue_proxy_wrapper.QueueProxyWrapper,
     controller: worker_controller.WorkerController,
 ) -> None:
     """
@@ -25,8 +28,15 @@ def video_input_worker(
     output_queue is the data queue.
     controller is how the main process communicates to this worker process.
     """
-    input_device = video_input.VideoInput(camera_name, save_name)
+    input_device = video_input.VideoInput(logging_queue, camera_name, save_name)
 
+    try:
+        frame = inspect.currentframe()
+        multiprocess_logging.log_message('video_input started', multiprocess_logging.DEBUG, frame, logging_queue)
+        # logging_queue.queue.put((multiprocess_logging.message_and_metadata('video_input started', frame), logging.DEBUG), block=False)
+    except queue.Full:
+        pass
+    
     while not controller.is_exit_requested():
         controller.check_pause()
 
