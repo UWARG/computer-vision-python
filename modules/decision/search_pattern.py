914fbd93780cd0c782a48cf6ed75d1cf1bb9548f
nsmartinx <nsmartinx@outlook.com>
2024-05-02 18:34:28 -0400

    formatting

--- modules/decision/search_pattern.py
+++ modules/decision/search_pattern.py
@@ -89,18 +89,19 @@ class SearchPattern:
             adjustment = (self.search_gap_width - self.search_gap_depth) / 2
 
         # Calculate the corners based on the offsets and the search origin
+        #In order: Top left, top right, bottom right, bottom left.
         self.square_corners = [
-            (self.search_origin_x - square_size - adjustment, self.search_origin_y + square_size),  # Top left corner
-            (self.search_origin_x + square_size, self.search_origin_y + square_size + adjustment),  # Top right corner
-            (self.search_origin_x + square_size + adjustment, self.search_origin_y - square_size),  # Bottom right corner
-            (self.search_origin_x - square_size, self.search_origin_y - square_size - adjustment),  # Bottom left corner
+            (self.search_origin_x - square_size - adjustment, self.search_origin_y + square_size),
+            (self.search_origin_x + square_size, self.search_origin_y + square_size + adjustment),
+            (self.search_origin_x + square_size + adjustment, self.search_origin_y - square_size),
+            (self.search_origin_x - square_size, self.search_origin_y - square_size - adjustment),
         ]
 
     def calculate_side_of_square(self):
         """
         Computes the gaps along the current side of the square
         """
-        # Calculate the current corner and the next corner to determine the current side's direction and length
+        # Calculate the current corner and the next corner
         self.current_corner = self.square_corners[self.current_side_in_square]
         next_corner = self.square_corners[(self.current_side_in_square + 1) % 4]
 
@@ -127,7 +128,7 @@ class SearchPattern:
         # If we've reached the end of the current side, move to the next side
         if self.current_pos_on_side >= self.max_pos_on_side:
             self.current_pos_on_side = -1  # Reset position counter for the new side
-            self.current_side_in_square = (self.current_side_in_square + 1) % 4  # Move to the next side
+            self.current_side_in_square = (self.current_side_in_square + 1) % 4
 
             if self.current_side_in_square == 0: # If completed this square
                 self.current_square += 1
