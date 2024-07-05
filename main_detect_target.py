2ae279c77a02a3e0e0282467810ed31f365567aa
Dylan Finlay <dylan.finlay33@gmail.com>
2024-07-05 17:00:04 -0400

    Added classical cv option to detect target and created worker test file

---
+++ main_detect_target.py
@@ -0,0 +1,171 @@
+"""
+For 2022-2023 UAS competition.
+"""
+
+import argparse
+import datetime
+import inspect
+import multiprocessing as mp
+import pathlib
+import queue
+
+import cv2
+import yaml
+
+from modules.detect_target import detect_target_worker
+from modules.video_input import video_input_worker
+from modules.logger import logger
+from utilities.workers import queue_proxy_wrapper
+from utilities.workers import worker_controller
+from utilities.workers import worker_manager
+
+
+CONFIG_FILE_PATH = pathlib.Path("config.yaml")
+
+
+# Code copied into main_2024.py
+# pylint: disable=duplicate-code
+def main() -> int:
+    """
+    Main function.
+    """
+    # Open config file
+    try:
+        with CONFIG_FILE_PATH.open("r", encoding="utf8") as file:
+            try:
+                config = yaml.safe_load(file)
+            except yaml.YAMLError as exc:
+                print(f"Error parsing YAML file: {exc}")
+                return -1
+    except FileNotFoundError:
+        print(f"File not found: {CONFIG_FILE_PATH}")
+        return -1
+    except IOError as exc:
+        print(f"Error when opening file: {exc}")
+        return -1
+
+    # Parse whether or not to force cpu from command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--cpu", action="store_true", help="option to force cpu")
+    parser.add_argument("--full", action="store_true", help="option to force full precision")
+    parser.add_argument(
+        "--show-annotated",
+        action="store_true",
+        help="option to show annotated image",
+    )
+    args = parser.parse_args()
+
+    # Set constants
+    try:
+        # Local constants
+        # pylint: disable=invalid-name
+        QUEUE_MAX_SIZE = config["queue_max_size"]
+
+        LOG_DIRECTORY_PATH = config["logger"]["directory_path"]
+        start_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        VIDEO_INPUT_CAMERA_NAME = config["video_input"]["camera_name"]
+        VIDEO_INPUT_WORKER_PERIOD = config["video_input"]["worker_period"]
+        VIDEO_INPUT_SAVE_NAME_PREFIX = config["video_input"]["save_prefix"]
+        VIDEO_INPUT_SAVE_PREFIX = (
+            f"{LOG_DIRECTORY_PATH}/{start_time}/{VIDEO_INPUT_SAVE_NAME_PREFIX}"
+        )
+
+        DETECT_TARGET_WORKER_COUNT = config["detect_target"]["worker_count"]
+        DETECT_TARGET_DEVICE = "cpu" if args.cpu else config["detect_target"]["device"]
+        DETECT_TARGET_MODEL_PATH = config["detect_target"]["model_path"]
+        DETECT_TARGET_OVERRIDE_FULL_PRECISION = args.full
+        DETECT_TARGET_SAVE_NAME_PREFIX = config["detect_target"]["save_prefix"]
+        DETECT_TARGET_SAVE_PREFIX = (
+            f"{LOG_DIRECTORY_PATH}/{start_time}/{DETECT_TARGET_SAVE_NAME_PREFIX}"
+        )
+        DETECT_TARGET_SHOW_ANNOTATED = args.show_annotated
+        # pylint: enable=invalid-name
+    except KeyError:
+        print("Config key(s) not found")
+        return -1
+
+    pathlib.Path(LOG_DIRECTORY_PATH).mkdir(exist_ok=True)
+    pathlib.Path(f"{LOG_DIRECTORY_PATH}/{start_time}").mkdir()
+
+    result, main_logger = logger.Logger.create("main")
+    if result:
+        frame = inspect.currentframe()
+        main_logger.info("main logger initialized", frame)
+
+    # Setup
+    controller = worker_controller.WorkerController()
+
+    mp_manager = mp.Manager()
+    video_input_to_detect_target_queue = queue_proxy_wrapper.QueueProxyWrapper(
+        mp_manager,
+        QUEUE_MAX_SIZE,
+    )
+    detect_target_to_main_queue = queue_proxy_wrapper.QueueProxyWrapper(
+        mp_manager,
+        QUEUE_MAX_SIZE,
+    )
+
+    video_input_manager = worker_manager.WorkerManager()
+    video_input_manager.create_workers(
+        1,
+        video_input_worker.video_input_worker,
+        (
+            VIDEO_INPUT_CAMERA_NAME,
+            VIDEO_INPUT_WORKER_PERIOD,
+            VIDEO_INPUT_SAVE_PREFIX,
+            video_input_to_detect_target_queue,
+            controller,
+        ),
+    )
+
+    detect_target_manager = worker_manager.WorkerManager()
+    detect_target_manager.create_workers(
+        DETECT_TARGET_WORKER_COUNT,
+        detect_target_worker.detect_target_worker,
+        (
+            DETECT_TARGET_DEVICE,
+            DETECT_TARGET_MODEL_PATH,
+            DETECT_TARGET_OVERRIDE_FULL_PRECISION,
+            DETECT_TARGET_SHOW_ANNOTATED,
+            DETECT_TARGET_SAVE_PREFIX,
+            video_input_to_detect_target_queue,
+            detect_target_to_main_queue,
+            controller,
+        ),
+    )
+
+    # Run
+    video_input_manager.start_workers()
+    detect_target_manager.start_workers()
+
+    while True:
+        detections_and_time = detect_target_to_main_queue.queue.get()
+        if detections_and_time is None:
+            break
+        print("Timestamp:" + str(detections_and_time.timestamp))
+        print("Num detections:" + str(len(detections_and_time.detections)))
+        for detection in detections_and_time.detections:
+            print("Detection:" + str(detection))
+
+    # Teardown
+    controller.request_exit()
+
+    video_input_to_detect_target_queue.fill_and_drain_queue()
+    detect_target_to_main_queue.fill_and_drain_queue()
+
+    video_input_manager.join_workers()
+    detect_target_manager.join_workers()
+
+    return 0
+
+
+# pylint: enable=duplicate-code
+
+
+if __name__ == "__main__":
+    result_main = main()
+    if result_main < 0:
+        print(f"ERROR: Status code: {result_main}")
+
+    print("Done!")
--- modules/detect_target/detect_target.py
+++ modules/detect_target/detect_target.py
@@ -4,7 +4,9 @@ Detects objects using the provided model.
 
 import time
 
+import copy
 import cv2
+import numpy as np
 import ultralytics
 
 from .. import image_and_time
@@ -19,6 +21,7 @@ class DetectTarget:
     def __init__(
         self,
         device: "str | int",
+        use_cv: bool,
         model_path: str,
         override_full: bool,
         show_annotations: bool = False,
@@ -32,6 +35,7 @@ class DetectTarget:
         save_name: filename prefix for logging detections and annotated images.
         """
         self.__device = device
+        self.__use_cv = use_cv
         self.__model = ultralytics.YOLO(model_path)
         self.__counter = 0
         self.__enable_half_precision = not self.__device == "cpu"
@@ -42,17 +46,99 @@ class DetectTarget:
         if save_name != "":
             self.__filename_prefix = save_name + "_" + str(int(time.time())) + "_"
 
-    def run(
-        self, data: image_and_time.ImageAndTime
-    ) -> "tuple[bool, detections_and_time.DetectionsAndTime | None]":
+    @staticmethod
+    def is_contour_circular(contour: np.ndarray) -> bool:
         """
-        Runs object detection on the provided image and returns the detections.
+        Checks if the shape is close to circular.
 
-        data: Image with a timestamp.
-
-        Return: Success and the detections.
+        Return: True is the shape is circular, false if it is not.
         """
-        image = data.image
+        CONTOUR_MINIMUM = 0.8
+        perimeter = cv2.arcLength(contour, True)
+
+        # Check if the perimeter is zero
+        if perimeter == 0.0:
+            return False
+
+        area = cv2.contourArea(contour)
+        circularity = 4 * np.pi * (area / (perimeter * perimeter))
+        return circularity > CONTOUR_MINIMUM
+
+    @staticmethod
+    def is_contour_large_enough(contour: np.ndarray, min_diameter: float) -> bool:
+        """
+        Checks if the shape is larger than the provided diameter.
+
+        Return: True if it is, false if it not.
+        """
+        _, radius = cv2.minEnclosingCircle(contour)
+        diameter = radius * 2
+        return diameter >= min_diameter
+
+    def detect_landing_pads_contours(self, image: "np.ndarray", timestamp: float) -> "tuple[bool, detections_and_time.DetectionsAndTime | None, np.ndarray]":
+        """
+        Detects landing pads using contours/classical cv.
+
+        image: Current image frame.
+        timestamp: Timestamp for the detections.
+
+        Return: Success, the DetectionsAndTime object, and the annotated image.
+        """
+        kernel = np.ones((2, 2), np.uint8)
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        threshold = 180
+        im_bw = cv2.threshold(gray_image, threshold, 255, cv2.THRESH_BINARY)[1]
+        im_dilation = cv2.dilate(im_bw, kernel, iterations=1)
+        contours, hierarchy = cv2.findContours(im_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) == 0:
+            return False, None, image
+
+        contours_with_children = set(i for i, hier in enumerate(hierarchy[0]) if hier[2] != -1)
+        parent_circular_contours = [
+            cnt
+            for i, cnt in enumerate(contours)
+            if self.is_contour_circular(cnt)
+            and self.is_contour_large_enough(cnt, 7)
+            and i in contours_with_children
+        ]
+
+        largest_contour = max(parent_circular_contours, key=cv2.contourArea, default=None)
+
+        if not largest_contour:
+            return False, None, image
+
+        # Create the DetectionsAndTime object
+        result, detections = detections_and_time.DetectionsAndTime.create(timestamp)
+        if not result:
+            return False, None, image
+
+        x, y, w, h = cv2.boundingRect(largest_contour)
+        bounds = np.array([x, y, x + w, y + h])
+        confidence = 1.0  # Confidence for classical CV is often set to a constant value
+        label = 0  # Label can be set to a constant or derived from some logic
+
+        # Create a Detection object and append it to detections
+        result, detection = detections_and_time.Detection.create(bounds, label, confidence)
+        if result:
+            detections.append(detection)
+
+        # Annotate the image
+        image_annotated = copy.deepcopy(image)
+        cv2.rectangle(image_annotated, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        return True, detections, image_annotated
+
+    def detect_landing_pads_yolo(self, image: "np.ndarray", timestamp: "float") -> "tuple[bool, detections_and_time.DetectionsAndTime | None, np.ndarray]":
+        """
+        Detects landing pads using YOLO model.
+
+        image: Current image frame.
+        timestamp: Timestamp for the detections.
+
+        Return: Success, the DetectionsAndTime object, and the annotated image.
+        """
         predictions = self.__model.predict(
             source=image,
             half=self.__enable_half_precision,
@@ -61,20 +147,20 @@ class DetectTarget:
         )
 
         if len(predictions) == 0:
-            return False, None
+            return False, None, image
 
         image_annotated = predictions[0].plot(conf=True)
 
         # Processing object detection
         boxes = predictions[0].boxes
         if boxes.shape[0] == 0:
-            return False, None
+            return False, None, image
 
         # Make a copy of bounding boxes in CPU space
         objects_bounds = boxes.xyxy.detach().cpu().numpy()
-        result, detections = detections_and_time.DetectionsAndTime.create(data.timestamp)
+        result, detections = detections_and_time.DetectionsAndTime.create(timestamp)
         if not result:
-            return False, None
+            return False, None, image
 
         # Get Pylance to stop complaining
         assert detections is not None
@@ -87,6 +173,28 @@ class DetectTarget:
             if result:
                 assert detection is not None
                 detections.append(detection)
+
+        return True, detections, image_annotated
+
+    def run(
+        self, data: image_and_time.ImageAndTime
+    ) -> "tuple[bool, detections_and_time.DetectionsAndTime | None]":
+        """
+        Runs object detection on the provided image and returns the detections.
+
+        data: Image with a timestamp.
+
+        Return: Success and the detections.
+        """
+        image = data.image
+        timestamp = data.timestamp
+
+        if self.__use_cv:
+            result, detections, image_annotated = self.detect_landing_pads_contours(image, timestamp)
+        else:
+            result, detections, image_annotated = self.detect_landing_pads_yolo(image, timestamp)
+        if not result:
+            return False, None
 
         # Logging
         if self.__filename_prefix != "":
