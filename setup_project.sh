cfae5e1cb7f2f606793477c2c342738248a002cd
Maxwell Lou <maxlou2005@gmail.com>
2024-08-22 20:59:45 -0400

    upgrade to python 3.11

---
+++ setup_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Initialize and update submodules script for Linux
+
+# Activate venv to prevent accidentally installing into global space
+source ./venv/bin/activate
+
+if [ $? -eq 0 ]; then
+    echo "Installing project dependencies..."
+    pip install -r requirements.txt
+    pip install -r requirements-pytorch.txt
+
+    echo ""
+    echo "Installing submodules and their dependencies..."
+    git submodule update --init --remote --recursive
+    git submodule foreach --recursive "pip install -r requirements.txt"
+
+    deactivate
+    echo ""
+    echo "Setup complete!"
+else
+    echo "Please install a virtual environment in the directory 'venv', at the project root directory"
+fi
