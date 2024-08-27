f321d756e238c55749fee1efdf1711f582d3b42a
trotyl15 <mail@trotyl.xyz>
2024-08-27 16:18:29 -0400

    format file

--- modules/flight_interface/flight_interface.py
+++ modules/flight_interface/flight_interface.py
@@ -12,7 +12,6 @@ from ..common.mavlink.modules import flight_controller
 from ..decision_command import decision_command
 
 
-
 class FlightInterface:
     """
     Create flight controller and combines odometry data and timestamp.
@@ -20,23 +19,28 @@ class FlightInterface:
 
     __create_key = object()
 
-
     @classmethod
     def create(
-        cls, address: str, timeout_home: float, baud_rate: int, local_logger: logger.Logger
+        cls,
+        address: str,
+        timeout_home: float,
+        baud_rate: int,
+        local_logger: logger.Logger,
     ) -> "tuple[bool, FlightInterface | None]":
         """
         address: TCP address or port.
         timeout_home: Timeout for home location in seconds.
         baud_rate: Baud rate for the connection.
         """
-        result, controller = flight_controller.FlightController.create(address, baud_rate)
+        result, controller = flight_controller.FlightController.create(
+            address, baud_rate
+        )
         if not result:
             frame = inspect.currentframe()
             local_logger.error("controller could not be created", frame)
             return False, None
-
         # Get Pylance to stop complaining
+
         assert controller is not None
 
         result, home_location = controller.get_home_location(timeout_home)
@@ -44,11 +48,13 @@ class FlightInterface:
             frame = inspect.currentframe()
             local_logger.error("home_location could not be created", frame)
             return False, None
-
         # Get Pylance to stop complaining
+
         assert home_location is not None
 
-        return True, FlightInterface(cls.__create_key, controller, home_location, local_logger)
+        return True, FlightInterface(
+            cls.__create_key, controller, home_location, local_logger
+        )
 
     def __init__(
         self,
@@ -60,7 +66,9 @@ class FlightInterface:
         """
         Private constructor, use create() method.
         """
-        assert class_private_create_key is FlightInterface.__create_key, "Use create() method"
+        assert (
+            class_private_create_key is FlightInterface.__create_key
+        ), "Use create() method"
 
         self.controller = controller
         self.__home_location = home_location
@@ -76,18 +84,20 @@ class FlightInterface:
         result, odometry = self.controller.get_odometry()
         if not result:
             return False, None
-
         # Get Pylance to stop complaining
+
         assert odometry is not None
 
-        result, odometry_local = local_global_conversion.drone_odometry_local_from_global(
-            odometry,
-            self.__home_location,
+        result, odometry_local = (
+            local_global_conversion.drone_odometry_local_from_global(
+                odometry,
+                self.__home_location,
+            )
         )
         if not result:
             return False, None
-
         # Get Pylance to stop complaining
+
         assert odometry_local is not None
 
         return odometry_and_time.OdometryAndTime.create(odometry_local)
