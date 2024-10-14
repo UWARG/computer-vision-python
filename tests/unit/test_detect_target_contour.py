b82364b79ba0c1776605217e5a75f2140edc746b
Zenkqi <SSGSSAchita@gmail.com>
2024-10-14 15:36:42 -0400

    Added corrections(?): Class -> Functions (Some sort of issue with detect_target)contour, extra unit tests added after)

--- tests/unit/test_detect_target_contour.py
+++ tests/unit/test_detect_target_contour.py
@@ -3,156 +3,119 @@ Test DetectTarget module.
 
 """
 
-import pathlib
-
+import math
 import cv2
 import numpy as np
 import pytest
-import math
 
 from modules.detect_target import detect_target_contour
 from modules import image_and_time
 from modules import detections_and_time
 
-
-TEST_PATH = pathlib.Path("tests", "model_example")
-BG_PATH = pathlib.Path(TEST_PATH, "background.png")
-
 BOUNDING_BOX_PRECISION_TOLERANCE = 0
 CONFIDENCE_PRECISION_TOLERANCE = 2
-
 
 # Test functions use test fixture signature names and access class privates
 # No enable
 # pylint: disable=protected-access,redefined-outer-name
 
 
-class GenerateTest:
-    def __init__(self, circle_data: list[list], image_file: str, txt_file: str) -> None:
-        self.circle_data = circle_data[0:]
-        self.image_file = image_file
-        self.txt_file = txt_file
-        self.image_path = "tests/model_example/"
+def blur_img(
+    bg: np.ndarray,
+    center: tuple[int, int],
+    radius: int = 0,
+    axis_length: tuple[int, int] = (0, 0),
+    angle: int = 0,
+    circle_check: bool = True,
+) -> np.ndarray:
+    """
+    Blurs an image a singular shape and adds it to the background.
+    """
 
-    def save_bounding_box_annotation(self, test_case: int, boxes_list: int) -> None:
-        """
-        Save the bounding box annotation for the circle in the format:
-        format: conf class_label x_min y_min x_max y_max
-        """
+    bg_copy = bg.copy()
+    x, y = bg_copy.shape[:2]
 
-        txt_file = self.image_path + self.txt_file + str(test_case) + ".txt"
-        with open(txt_file, "w") as f:
-            for class_label, (top_left, bottom_right) in enumerate(boxes_list):
-                x_min, y_min = top_left
-                x_max, y_max = bottom_right
+    mask = np.zeros((x, y), np.uint8)
+    if circle_check:
+        mask = cv2.circle(mask, center, radius, (215, 158, 115), -1, cv2.LINE_AA)
+    else:
+        mask = cv2.ellipse(mask, center, axis_length, angle, 0, 360, (215, 158, 115), -1)
 
-                f.write(f"{1} {class_label} {x_min} {y_min} {x_max} {y_max}\n")
-        print(f"Bounding box annotation saved to {txt_file}")
+    mask = cv2.blur(mask, (25, 25), 7)
 
-    def blur_img(
-        self,
-        bg: np.ndarray,
-        center: tuple[int, int],
-        radius: int = 0,
-        axis_length: tuple[int, int] = (0, 0),
-        angle: int = 0,
-        circle_check: bool = True,
-    ) -> np.ndarray:
-        """
-        Blurs an image a singular shape and adds it to the background.
-        """
+    alpha = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) / 255.0
+    fg = np.zeros(bg.shape, np.uint8)
+    fg[:, :, :] = [200, 10, 200]
 
-        bg_copy = bg.copy()
-        x, y = bg_copy.shape[:2]
+    blended = cv2.convertScaleAbs(bg * (1 - alpha) + fg * alpha)
+    return blended
 
-        mask = np.zeros((x, y), np.uint8)
-        if circle_check:
-            mask = cv2.circle(mask, center, radius, (215, 158, 115), -1, cv2.LINE_AA)
-        else:
-            mask = cv2.ellipse(mask, center, axis_length, angle, 0, 360, (215, 158, 115), -1)
 
-        mask = cv2.blur(mask, (25, 25), 7)
+def draw_circle(
+    image: np.ndarray, center: tuple[int, int], radius: int, blur: bool
+) -> tuple[np.ndarray, int, int]:
+    """
+    Draws a circle on the provided image and saves the bounding box coordinates to a text file.
+    """
+    x, y = center
+    top_left = (max(x - radius, 0), max(y - radius, 0))
+    bottom_right = (min(x + radius, image.shape[1]), min(y + radius, image.shape[0]))
 
-        alpha = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) / 255.0
-        fg = np.zeros(bg.shape, np.uint8)
-        fg[:, :, :] = [200, 10, 200]
-
-        blended = cv2.convertScaleAbs(bg * (1 - alpha) + fg * alpha)
-        return blended
-
-    def draw_circle(
-        self, image: np.ndarray, center: tuple[int, int], radius: int, blur: bool
-    ) -> tuple[np.ndarray, int, int]:
-        """
-        Draws a circle on the provided image and saves the bounding box coordinates to a text file.
-        """
-        x, y = center
-        top_left = (max(x - radius, 0), max(y - radius, 0))
-        bottom_right = (min(x + radius, image.shape[1]), min(y + radius, image.shape[0]))
-
-        if blur:
-            image = self.blur_img(image, center, radius=radius, circle_check=True)
-            return image, top_left, bottom_right
-
-        cv2.circle(image, center, radius, (215, 158, 115), -1)
+    if blur:
+        image = blur_img(image, center, radius=radius, circle_check=True)
         return image, top_left, bottom_right
 
-    def draw_ellipse(
-        self, image: np.ndarray, center: tuple[int, int], axis_length: tuple, angle: int, blur: bool
-    ) -> tuple[np.ndarray, int, int]:
-        """
-        Draws an ellipse on the provided image and saves the bounding box coordinates to a text file.
-        """
+    cv2.circle(image, center, radius, (215, 158, 115), -1)
+    return image, top_left, bottom_right
 
-        (h, k), (a, b) = center, axis_length
-        rad = math.pi / 180
-        ux, uy = a * math.cos(angle * rad), a * math.sin(angle * rad)  # first point on the ellipse
-        vx, vy = b * math.sin(angle * rad), b * math.cos(angle * rad)
-        width, height = 2 * math.sqrt(ux**2 + vx**2), 2 * math.sqrt(uy**2 + vy**2)
 
-        top_left = (int(max(h - (0.5) * width, 0)), int(max(k - (0.5) * height, 0)))
-        bottom_right = (
-            int(min(h + (0.5) * width, image.shape[1])),
-            int(min(k + (0.5) * height, image.shape[0])),
-        )
+def draw_ellipse(
+    image: np.ndarray, center: tuple[int, int], axis_length: tuple, angle: int, blur: bool
+) -> tuple[np.ndarray, int, int]:
+    """
+    Draws an ellipse on the provided image and saves the bounding box coordinates to a text file.
+    """
+    (h, k), (a, b) = center, axis_length
+    rad = math.pi / 180
+    ux, uy = a * math.cos(angle * rad), a * math.sin(angle * rad)  # first point on the ellipse
+    vx, vy = b * math.sin(angle * rad), b * math.cos(angle * rad)
+    width, height = 2 * math.sqrt(ux**2 + vx**2), 2 * math.sqrt(uy**2 + vy**2)
 
-        if blur:
-            image = self.blur_img(
-                image, center, axis_length=axis_length, angle=angle, circle_check=False
-            )
-            return image, top_left, bottom_right
+    top_left = (int(max(h - (0.5) * width, 0)), int(max(k - (0.5) * height, 0)))
+    bottom_right = (
+        int(min(h + (0.5) * width, image.shape[1])),
+        int(min(k + (0.5) * height, image.shape[0])),
+    )
 
-        image = cv2.ellipse(image, center, axis_length, angle, 0, 360, (215, 158, 115), -1)
+    if blur:
+        image = blur_img(image, center, axis_length=axis_length, angle=angle, circle_check=False)
         return image, top_left, bottom_right
 
-    def create_test_case(self, test_case: int) -> tuple[str, str]:
-        """
-        Genereates test cases given a data set.
-        """
-        image = cv2.imread(self.image_file)
-
-        boxes_list = []
-        for center, radius, blur, ellipse_data in self.circle_data:
-            if ellipse_data[0]:
-                _, axis_length, angle = ellipse_data
-                image, top_left, bottom_right = self.draw_ellipse(
-                    image, center, axis_length, angle, blur
-                )
-                boxes_list.append((top_left, bottom_right))
-                continue
-
-            image, top_left, bottom_right = self.draw_circle(image, center, radius, blur)
-            boxes_list.append((top_left, bottom_right))
-
-        self.save_bounding_box_annotation(test_case, boxes_list)
-
-        output_image_file = f"{self.image_path}test_output_{test_case}.png"
-        cv2.imwrite(output_image_file, image)
-        print(f"Image with bounding box saved as {output_image_file}")
-        return (output_image_file, self.image_path + self.txt_file + f"{test_case}.txt")
+    image = cv2.ellipse(image, center, axis_length, angle, 0, 360, (215, 158, 115), -1)
+    return image, top_left, bottom_right
 
 
-# ---------------------------------------------------------------------------------------------------------------------
+def create_test_case(
+    circle_data: list[tuple[int, int], int, bool, list[bool, tuple[int, int] | None, int | None]]
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Genereates test cases given a data set.
+    """
+    image = np.zeros(shape=(800, 1800, 3), dtype=np.int16)
+
+    boxes_list = []
+    for center, radius, blur, ellipse_data in circle_data:
+        if ellipse_data[0]:
+            _, axis_length, angle = ellipse_data
+            image, top_left, bottom_right = draw_ellipse(image, center, axis_length, angle, blur)
+            boxes_list.append([1, 0] + [point for point in top_left + bottom_right])
+            continue
+
+        image, top_left, bottom_right = draw_circle(image, center, radius, blur)
+        boxes_list.append([1, 0] + [point for point in top_left + bottom_right])
+
+    boxes_list = np.array(boxes_list)
+    return (image.astype(np.uint8), boxes_list)
 
 
 def compare_detections(
@@ -225,62 +188,64 @@ def create_detections(detections_from_file: np.ndarray) -> detections_and_time.D
     return detections
 
 
-# ------------------------------------------------------------------------------------------------------------------
 @pytest.fixture()
 def detector() -> detect_target_contour.DetectTargetContour:  # type: ignore
     """
-    Construct DetectTargetUltralytics.
+    Construct DetectTargetContour.
     """
     detection = detect_target_contour.DetectTargetContour()
     yield detection  # type: ignore
 
 
-# ---------------------------------------------------------------------------
+@pytest.fixture()
+def image_easy() -> image_and_time.ImageAndTime:  # type: ignore
+    """
+    Load easy image.
+    """
+
+    circle_data = [[(1000, 400), 200, False, [False, None, None]]]
+
+    image = create_test_case(circle_data)
+    result, actual_image = image_and_time.ImageAndTime.create(image[0])
+    print((result, actual_image))
+    assert result
+    assert actual_image is not None
+    yield actual_image  # type: ignore
+
+
+@pytest.fixture()
+def expected_easy() -> image_and_time.ImageAndTime:  # type: ignore
+    """
+    Load expected an easy image detections.
+    """
+
+    circle_data = [
+        [(500, 1000), 400, False, [False, None, None]],
+    ]
+
+    _, expected = create_test_case(circle_data)
+    yield create_detections(expected)  # type: ignore
+
+
 class TestDetector:
     """
     Tests `DetectTarget.run()` .
     """
 
-    def test_multiple_landing_pads(
+    def test_single_circle(
         self,
         detector: detect_target_contour.DetectTargetContour,
+        image_easy: image_and_time.ImageAndTime,
+        expected_easy: detections_and_time.DetectionsAndTime,
     ) -> None:
         """
-        Multiple images.
+        Bus image.
         """
-
-        circle_data = [
-            [(200, 200), 400, False, [False, None, None]],
-            [(1500, 700), 500, False, [False, None, None]],
-        ]
-
-        actual_detections, expected_detections = [], []
-        circle_list = [circle_data]
-
-        for i, circle_data in enumerate(circle_list):
-            generate_test = GenerateTest(circle_data, BG_PATH, "bounding_box")
-            image_file, txt_file = generate_test.create_test_case(i + 1)
-            image = cv2.imread(image_file, 1)
-
-            result, actual = image_and_time.ImageAndTime.create(image)
-            assert result
-            assert actual is not None
-
-            expected = create_detections(np.loadtxt(txt_file))
-            actual_detections.append(actual)
-            expected_detections.append(expected)
-
         # Run
-        outputs = []
-        for i in range(0, len(circle_list)):
-            output = detector.run(actual_detections[i])
-            outputs.append(output)
+        result, actual = detector.run(image_easy)
 
-        print(outputs)
         # Test
-        for i in range(0, len(outputs)):
-            output: "tuple[bool, detections_and_time.DetectionsAndTime | None]" = outputs[i]
-            result, actual = output
+        assert result
+        assert actual is not None
 
-            print(actual)
-            compare_detections(actual, expected_detections[i])
+        compare_detections(actual, expected_easy)
