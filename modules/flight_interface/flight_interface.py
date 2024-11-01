024b3044d6f2f7411b4fedeee7b820b1211bad5a
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-11-01 14:52:35 -0400

    Put imports on one line

--- modules/flight_interface/flight_interface.py
+++ modules/flight_interface/flight_interface.py
@@ -4,12 +4,7 @@ Creates flight controller and combines odometry data and timestamp.
 
 from .. import decision_command, odometry_and_time
 from ..common.logger.modules import logger
-from ..common.mavlink.modules import (
-    drone_odometry,
-    drone_odometry_local,
-    flight_controller,
-    local_global_conversion,
-)
+from ..common.mavlink.modules import drone_odometry, drone_odometry_local, flight_controller, local_global_conversion  # fmt: skip
 
 
 class FlightInterface:
