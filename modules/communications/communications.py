9d3b588a579af86333eaa55fc935bed5c0486688
Maxwell Lou <maxlou2005@gmail.com>
2024-11-01 23:16:31 -0400

    Modify home location to be part of create for flight interface and communication worker

--- modules/communications/communications.py
+++ modules/communications/communications.py
@@ -1,4 +1,5 @@
 import time
+from ..common.mavlink.modules import drone_odometry
 from modules.common.logger.modules import logger
 from modules.common.mavlink.modules.drone_odometry import DronePosition
 from modules.detection_in_world import DetectionInWorld
@@ -6,24 +7,26 @@ from modules.flight_interface.local_global_conversion import detection_in_world_
 
 
 class Communications:
-    """
-    """
+    """ """
+
     __create_key = object()
 
     @classmethod
     def create(
         cls,
+        home_location: drone_odometry.DronePosition,
         local_logger: logger.Logger,
     ) -> "tuple[bool, Communications | None]":
         """
         Logs data and forwards it.
         """
 
-        return True, Communications(cls.__create_key, local_logger)
+        return True, Communications(cls.__create_key, home_location, local_logger)
 
     def __init__(
         self,
         class_private_create_key: object,
+        home_location: drone_odometry.DronePosition,
         local_logger: logger.Logger,
     ) -> None:
         """
@@ -31,20 +34,27 @@ class Communications:
         """
         assert class_private_create_key is Communications.__create_key, "Use create() method"
 
+        self.__home_location = home_location
         self.__logger = local_logger
 
     def run(
-        self, detections_in_world: list[DetectionInWorld], home_location: DronePosition
+        self, detections_in_world: list[DetectionInWorld]
     ) -> tuple[bool, list[DetectionInWorld] | None]:
+
+        detections_in_world_global = []
         for detection_in_world in detections_in_world:
             result, detection_in_world_global = detection_in_world_global_from_local(
-                home_location, detection_in_world
+                self.__home_location, detection_in_world
             )
 
             if not result:
+                # Log nothing if at least one of the conversions failed
                 self.__logger.error("conversion failed")
                 return False, detections_in_world
 
-            self.__logger.info(str(time.time()) + ": " + str(detection_in_world_global))
+            detections_in_world_global.append(detection_in_world_global)
+
+        timestamp = time.time()
+        self.__logger.info(f"{timestamp}: {detections_in_world_global}")
 
         return True, detections_in_world
