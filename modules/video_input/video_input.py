a268391269899cfa28f77a711e8949b6f7d18523
Aleksa-M <a2misic@uwaterloo.ca>
2024-12-22 00:11:13 -0500

    integrated camera_factory in video_input (#232)
    * integrated camera_factory in video_input
    
    * added missing parameters
    
    * removed cluster estimation test file
    
    * accommodate video input for new camera factory and logger changes in common
    
    * refactor for moving camera config classes into each camera module
    
    * add comments and change empty to string to a valid iamge prefix
    
    * Fix comments
    
    * refactor video_input save_prefix to image_name, for proper use with logger.save_image
    
    * bug fix for video_input integration tests
    
    ---------
    
    Co-authored-by: Maxwell Lou <maxlou2005@gmail.com>

--- modules/video_input/video_input.py
+++ modules/video_input/video_input.py
@@ -3,7 +3,11 @@ Combines image and timestamp together.
 """
 
 from .. import image_and_time
-from ..common.modules.camera import camera_device
+from ..common.modules.camera import base_camera
+from ..common.modules.camera import camera_factory
+from ..common.modules.camera import camera_opencv
+from ..common.modules.camera import camera_picamera2
+from ..common.modules.logger import logger
 
 
 class VideoInput:
@@ -11,15 +15,60 @@ class VideoInput:
     Combines image and timestamp together.
     """
 
-    def __init__(self, camera_name: "int | str", save_name: str = "") -> None:
-        self.device = camera_device.CameraDevice(camera_name, 1, save_name)
+    __create_key = object()
 
-    def run(self) -> "tuple[bool, image_and_time.ImageAndTime | None]":
+    @classmethod
+    def create(
+        cls,
+        camera_option: camera_factory.CameraOption,
+        width: int,
+        height: int,
+        config: camera_opencv.ConfigOpenCV | camera_picamera2.ConfigPiCamera2,
+        maybe_image_name: str | None,
+        local_logger: logger.Logger,
+    ) -> "tuple[True, VideoInput] | tuple[False, None]":
+        """
+        camera_option specifies which camera driver to use.
+        width is the width of the images the camera takes in pixels.
+        height is the height of the images the camera takes in pixels.
+        camera_config specifies camera settings.
+        maybe_image_name is the filename to save the image as. Set to None to not log any images.
+        """
+        result, camera = camera_factory.create_camera(camera_option, width, height, config)
+        if not result:
+            local_logger.error(
+                f"Camera factory failed. Current configs were: {camera_option=}, {width=}, {height=}, {config=}."
+            )
+            return False, None
+
+        return True, VideoInput(cls.__create_key, camera, maybe_image_name, local_logger)
+
+    def __init__(
+        self,
+        class_private_create_key: object,
+        camera: base_camera.BaseCameraDevice,
+        maybe_image_name: str | None,
+        local_logger: logger.Logger,
+    ) -> None:
+        """
+        Private constructor, use create() method.
+        """
+        assert class_private_create_key is VideoInput.__create_key, "Use create() method."
+
+        self.__device = camera
+        self.__maybe_image_name = maybe_image_name
+        self.__logger = local_logger
+
+    def run(self) -> "tuple[True, image_and_time.ImageAndTime] | tuple[False, None]":
         """
         Returns a possible ImageAndTime with current timestamp.
         """
-        result, image = self.device.get_image()
+        result, image = self.__device.run()
         if not result:
+            self.__logger.warning("Failed to take image")
             return False, None
+
+        if self.__maybe_image_name is not None:
+            self.__logger.save_image(image, self.__maybe_image_name)
 
         return image_and_time.ImageAndTime.create(image)
--- tests/integration/test_video_input_worker.py
+++ tests/integration/test_video_input_worker.py
@@ -6,14 +6,21 @@ import multiprocessing as mp
 import queue
 import time
 
+from modules.common.modules.camera import camera_factory
+from modules.common.modules.camera import camera_opencv
 from modules.video_input import video_input_worker
 from modules import image_and_time
 from utilities.workers import queue_proxy_wrapper
 from utilities.workers import worker_controller
 
 
+# Modify these settings as needed
 VIDEO_INPUT_WORKER_PERIOD = 1.0
-CAMERA = 0
+CAMERA = camera_factory.CameraOption.OPENCV
+WIDTH = 640
+HEIGHT = 480
+CONFIG = camera_opencv.ConfigOpenCV(0)
+IMAGE_NAME = None  # Not saving any pictures
 
 
 def main() -> int:
@@ -29,7 +36,16 @@ def main() -> int:
 
     worker = mp.Process(
         target=video_input_worker.video_input_worker,
-        args=(CAMERA, VIDEO_INPUT_WORKER_PERIOD, "", out_queue, controller),
+        args=(
+            CAMERA,
+            WIDTH,
+            HEIGHT,
+            CONFIG,
+            IMAGE_NAME,
+            VIDEO_INPUT_WORKER_PERIOD,
+            out_queue,
+            controller,
+        ),
     )
 
     # Run
