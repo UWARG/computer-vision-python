3954351cf0d8e87881394bf873ff7bd5fdeb7d41
Tochi Okoro <ellyokes253@gmail.com>
2024-12-29 00:06:23 -0700

    added files

---
+++ modules/auto_landing/auto_landing.py
@@ -0,0 +1,14 @@
+"""
+Auto-landing script.
+"""
+
+class AutoLanding:
+    def __init__ (self):
+        raise NotImplementedError
+    
+    def run (self):
+        raise NotImplementedError
+    
+    def calculate_angles (self):
+        raise NotImplementedError
+    
