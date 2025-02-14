5b25de846289be44808587d44397ce29664f6be1
Bill <szbillyang@gmail.com>
2025-02-13 20:49:25 -0500

    format

--- modules/cluster_estimation/cluster_estimation.py
+++ modules/cluster_estimation/cluster_estimation.py
@@ -13,6 +13,7 @@ from .. import object_in_world
 from .. import detection_in_world
 from ..common.modules.logger import logger
 
+
 # pylint: disable=too-many-instance-attributes
 class ClusterEstimation:
     """
