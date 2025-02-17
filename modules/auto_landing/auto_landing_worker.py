b5265652b95909e8926f135918b956f1beed83a8
ellyokes253 <tookoro@uwaterloo.ca>
2025-02-16 23:19:33 -0500

    Autonomous Landing Module (#235)
    * intial commit
    
    * added files
    
    * removed old module
    
    * made class
    
    * made changes
    
    * added worker
    
    * made changes
    
    * issues commiting, now fixed
    
    * made changes
    
    * linter issues fixed
    
    * fixing linter issues
    
    * fixing code and linter issues
    
    * continuation of debugging
    
    * fixing linter issues
    
    * fixing logger initialization
    
    * fixing last linter issues
    
    * last few linter issues
    
    * last few changes
    
    * updated changes
    
    * fixing small linter issue
    
    * fixed run()
    
    * pulling new commit changes in common
    
    * fixing linter error
    
    * using first bbox
    
    * added AutoLandingInformation object
    
    * fixing small mistakes
    
    * redefined AutoLandingInformation class
    
    * added changes
    
    * fixed linter issues

---
+++ modules/auto_landing/auto_landing_worker.py
@@ -0,0 +1,66 @@
+"""
+Auto-landing worker.
+"""
+
+import os
+import pathlib
+import time
+
+from utilities.workers import queue_proxy_wrapper
+from utilities.workers import worker_controller
+from . import auto_landing
+from ..common.modules.logger import logger
+
+
+def auto_landing_worker(
+    fov_x: float,
+    fov_y: float,
+    im_h: float,
+    im_w: float,
+    period: float,
+    input_queue: queue_proxy_wrapper.QueueProxyWrapper,
+    output_queue: queue_proxy_wrapper.QueueProxyWrapper,
+    controller: worker_controller.WorkerController,
+) -> None:
+    """
+    Worker process.
+
+    period: Wait time in seconds.
+    input_queue and output_queue are data queues.
+    controller is how the main process communicates to this worker process.
+    """
+
+    worker_name = pathlib.Path(__file__).stem
+    process_id = os.getpid()
+    result, local_logger = logger.Logger.create(f"{worker_name}_{process_id}", True)
+    if not result:
+        print("ERROR: Worker failed to create logger")
+        return
+
+    # Get Pylance to stop complaining
+    assert local_logger is not None
+
+    local_logger.info("Logger initialized", True)
+
+    result, auto_lander = auto_landing.AutoLanding.create(fov_x, fov_y, im_h, im_w, local_logger)
+
+    if not result:
+        local_logger.error("Worker failed to create class object", True)
+        return
+
+    # Get Pylance to stop complaining
+    assert auto_lander is not None
+
+    while not controller.is_exit_requested():
+        controller.check_pause()
+
+        input_data = input_queue.queue.get()
+        if input_data is None:
+            continue
+
+        result, value = auto_lander.run(input_data)
+        if not result:
+            continue
+
+        output_queue.queue.put(value)
+        time.sleep(period)
